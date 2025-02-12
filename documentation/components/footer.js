3c3b7966c5a71d89ec0445b19a488db3ccc3553f
Thoralf Müller <thoralf.mue@gmail.com>
2025-02-12 12:52:59 +0100

    Address review comments

--- documentation/components/footer.js
+++ documentation/components/footer.js
@@ -7,11 +7,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-blurple pb-[env(safe-area-inset-bottom)] print:bg-transparent">
-      <hr className="dark:border-blurpleDark"></hr>  
+      <hr className="dark:border-blurpleDark"></hr>
       <div className="mx-auto flex max-w-[90rem] justify-center py-12 text-white md:justify-start pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
         Copyright {new Date().getFullYear()} ©{" "}
         <a href="https://iota.org" target="_blank">
-        , Mysten Labs, Inc.
+          , IOTA Stiftung
         </a>
       </div>
     </footer>
