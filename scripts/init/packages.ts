7b047ec488fe1c95ceb85a4fd4d339583d1b33fe
/alex/ <alexander.schmidt@iota.org>
2025-02-24 10:25:53 +0100

    use framework/devnet branch for localnet deployment

--- scripts/init/packages.ts
+++ scripts/init/packages.ts
@@ -37,7 +37,7 @@ const parseCreatedObject = (data: IotaTransactionBlockResponse, objectType: stri
 };
 
 export const Packages = (network: Network) => {
-	const rev = network === 'localnet' ? 'main' : `framework/${network}`;
+	const rev = network === 'localnet' ? 'framework/devnet' : `framework/${network}`;
 	const subdomainExtraDependencies = `denylist = { local = "../denylist" }`;
 
 	return {
