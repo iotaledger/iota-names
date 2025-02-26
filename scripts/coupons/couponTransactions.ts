MystenLabs/suins-contracts

SDK, Payments Tests, Package Management (#241)
* sdk changes

* update using new free claims

* lockfile update

* action update'

* authorize free claims

* new package constants

* lockfile updates

* upgrade payment dependency

* new move packages

* actions for authorizing packages

* authorize package action

* cleanup

* sdk

* changeset

* deauthorize old packages

scripts/coupons/couponTransactions.ts
@@ -1,0 +1,35 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Transaction, TransactionObjectArgument } from '@mysten/sui/transactions';
+
+import { mainPackage, Network } from '../config/constants';
+import { signAndExecute } from '../utils/utils';
+import { CouponType } from './coupon';
+
+const network = (process.env.NETWORK as Network) || 'testnet';
+const config = mainPackage[network];
+
+export const createCoupon = () => {
+	const tx = new Transaction();
+	const couponConfig = new CouponType(100, 0); // type = 0 for percentage discount, 15% off
+	// couponConfig.setName('fiveplus15percentoff');
+	couponConfig.setName('100percentoff');
+	couponConfig.setLengthRule([5, 63]);
+	couponConfig.setAvailableClaims(100);
+	couponConfig.setYears([1, 3]);
+	couponConfig.setExpiration('1800000000000');
+	couponConfig.toTransaction(tx, config);
+
+	return signAndExecute(tx, network);
+};
+
+export const applyCoupon =
+	(intent: TransactionObjectArgument, couponCode: string) => (tx: Transaction) => {
+		return tx.moveCall({
+			target: `${config.coupons.packageId}::coupon_house::apply_coupon`,
+			arguments: [tx.object(config.suins), intent, tx.pure.string(couponCode), tx.object.clock()],
+		});
+	};
+
+// createCoupon();

scripts/discounts/discounts.ts
@@ -1,0 +1,74 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Transaction, TransactionObjectArgument } from '@mysten/sui/transactions';
+
+import { mainPackage, Network } from '../config/constants';
+import { setupFreeClaimsForType } from '../config/discounts';
+import { newPriceConfigV2 } from '../init/authorization';
+import { getObjectType, signAndExecute } from '../utils/utils';
+
+const network = (process.env.NETWORK as Network) || 'testnet';
+const config = mainPackage[network];
+
+export const authorizeDiscountType = (type: string) => {
+	const tx = new Transaction();
+	tx.moveCall({
+		target: `${config.discountsPackage.packageId}::discounts::authorize_type`,
+		arguments: [
+			tx.object(config.discountsPackage.discountHouseId),
+			tx.object(config.adminCap),
+			newPriceConfigV2({
+				txb: tx,
+				packageId: config.packageId,
+				ranges: [
+					[3, 3],
+					[4, 4],
+					[5, 63],
+				],
+				prices: [30, 30, 30], // Discount percentages
+			}),
+		],
+		typeArguments: [type],
+	});
+
+	return signAndExecute(tx, network);
+};
+
+export const applyDiscount = async (
+	intent: TransactionObjectArgument,
+	discountNft: string,
+	network: Network,
+	tx: Transaction,
+) => {
+	const discountNftType = await getObjectType(network, discountNft);
+
+	tx.moveCall({
+		target: `${config.discountsPackage.packageId}::discounts::apply_percentage_discount`,
+		arguments: [
+			tx.object(config.discountsPackage.discountHouseId),
+			intent,
+			tx.object(config.suins),
+			tx.object(discountNft),
+		],
+		typeArguments: [discountNftType],
+	});
+};
+
+// authorizeDiscountType(
+// 	'0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::suifrens::SuiFren<0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::capy::Capy>',
+// );
+
+const setupFreeClaim = () => {
+	const tx = new Transaction();
+	setupFreeClaimsForType(
+		tx,
+		config,
+		'0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::suifrens::SuiFren<0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::capy::Capy>',
+		{ from: 4, to: 5 },
+	);
+
+	return signAndExecute(tx, network);
+};
+
+// setupFreeClaim();

scripts/init/authorization.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-import { TransactionArgument, type Transaction } from '@mysten/sui/transactions';
+import { Transaction, TransactionArgument } from '@mysten/sui/transactions';
+import { hexToBytes } from '@noble/hashes/utils';
 
 /**
  * A helper to authorize any app in the SuiNS object.
@@ -121,7 +122,7 @@ export const removeConfig = ({
 /**
  * Creates a default `config` which saves the price list and public key.
  */
-export const newPriceConfig = ({
+export const newPriceConfigV1 = ({
 	txb,
 	suinsPackageIdV1,
 	priceList,
@@ -140,6 +141,173 @@ export const newPriceConfig = ({
 			txb.pure.u64(priceList.four),
 			txb.pure.u64(priceList.fivePlus),
 		],
+	});
+};
+
+export const newPriceConfigV2 = ({
+	txb,
+	packageId,
+	ranges,
+	prices,
+}: {
+	txb: Transaction;
+	packageId: string;
+	ranges: number[][];
+	prices: number[];
+}): TransactionArgument => {
+	var rangesList: TransactionArgument[] = [];
+	for (const range of ranges) {
+		if (range.length !== 2) {
+			throw new Error('Each range must have exactly 2 elements');
+		}
+		rangesList.push(newRange({ txb, packageId, range }));
+	}
+	return txb.moveCall({
+		target: `${packageId}::pricing_config::new`,
+		arguments: [
+			txb.makeMoveVec({ elements: rangesList, type: `${packageId}::pricing_config::Range` }),
+			txb.pure.vector('u64', prices),
+		],
+	});
+};
+
+export const addCoreConfig = ({
+	txb,
+	latestPackageId,
+}: {
+	txb: Transaction;
+	latestPackageId: string;
+}) => {
+	return txb.moveCall({
+		target: `${latestPackageId}::core_config::new`,
+		arguments: [
+			txb.pure.vector('string', []),
+			txb.pure.u8(3),
+			txb.pure.u8(63),
+			txb.pure.u8(1),
+			txb.pure.u8(5),
+			txb.pure.vector('string', ['sui']),
+			txb.moveCall({
+				target: '0x2::vec_map::empty',
+				typeArguments: ['0x1::string::String', '0x1::string::String'],
+			}),
+		],
+	});
+};
+
+export const newRenewalConfig = ({
+	txb,
+	packageId,
+	ranges,
+	prices,
+}: {
+	txb: Transaction;
+	packageId: string;
+	ranges: number[][];
+	prices: number[];
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: `${packageId}::pricing_config::new_renewal_config`,
+		arguments: [newPriceConfigV2({ txb, packageId, ranges, prices })],
+	});
+};
+
+export const newRange = ({
+	txb,
+	packageId,
+	range,
+}: {
+	txb: Transaction;
+	packageId: string;
+	range: number[];
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: `${packageId}::pricing_config::new_range`,
+		arguments: [txb.pure.vector('u64', range)],
+	});
+};
+
+export const newPaymentsConfig = ({
+	txb,
+	packageId,
+	coinTypeAndDiscount,
+	baseCurrencyType,
+	maxAge,
+}: {
+	txb: Transaction;
+	packageId: string;
+	coinTypeAndDiscount: [Record<string, string>, number][]; // Array of [{type: string, metadataId: string, feed: string}, discountPercentage] pairs
+	baseCurrencyType: string;
+	maxAge: number;
+}): TransactionArgument => {
+	const coinTypeDataList: TransactionArgument[] = [];
+
+	for (const [coin, discountPercentage] of coinTypeAndDiscount) {
+		coinTypeDataList.push(
+			newCoinTypeData({
+				txb,
+				packageId,
+				discountPercentage,
+				coinType: coin['type'],
+				coinMetadataId: coin['metadataId'],
+				priceFeed: coin['feed']
+					? new Uint8Array(
+							hexToBytes(coin['feed'].startsWith('0x') ? coin['feed'].slice(2) : coin['feed']),
+						)
+					: new Uint8Array(),
+			}),
+		);
+	}
+
+	return txb.moveCall({
+		target: `${packageId}::payments::new_payments_config`,
+		arguments: [
+			txb.makeMoveVec({
+				elements: coinTypeDataList,
+				type: `${packageId}::payments::CoinTypeData`,
+			}),
+			getTypeName({ txb, coinType: baseCurrencyType }),
+			txb.pure.u64(maxAge),
+		],
+	});
+};
+
+export const getTypeName = ({
+	txb,
+	coinType,
+}: {
+	txb: Transaction;
+	coinType: string;
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: '0x1::type_name::get',
+		typeArguments: [coinType],
+	});
+};
+
+export const newCoinTypeData = ({
+	txb,
+	packageId,
+	discountPercentage,
+	coinType,
+	coinMetadataId,
+	priceFeed,
+}: {
+	txb: Transaction;
+	packageId: string;
+	discountPercentage: number;
+	coinType: string;
+	coinMetadataId: string;
+	priceFeed: Uint8Array;
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: `${packageId}::payments::new_coin_type_data`,
+		arguments: [
+			txb.object(coinMetadataId),
+			txb.pure.u8(discountPercentage),
+			txb.pure.vector('u8', priceFeed),
+		],
+		typeArguments: [coinType],
 	});
 };
 

scripts/init/publish.ts
@@ -16,11 +16,13 @@ export const publishPackages = async (network: Network, isCiJob = false, configP
 	// split by ordering, and publish in batch.
 	const orderings = [...new Set([...Object.values(packages).map((x) => x.order)])];
 
-	// We do the publishing in batches, because some
+	// We do the publishing in batches, because some needs to be published before others
 	for (const ordering of orderings) {
 		const list = Object.entries(packages).filter((x) => x[1].order === ordering);
 
 		for (const [key, pkg] of list) {
+			console.log(`Publishing ${key}...`);
+			console.log(`Package folder: ${pkg.folder}`);
 			const packageFolder = path.resolve(contractsPath, pkg.folder);
 			const manifestFile = path.resolve(packageFolder + '/Move.toml');
 			// remove the lockfile on CI to allow fresh flows.
@@ -32,7 +34,6 @@ export const publishPackages = async (network: Network, isCiJob = false, configP
 					console.info('Lock file removed');
 				}
 			}
-
 			writeFileSync(manifestFile, pkg.manifest()); // save the manifest as is.
 
 			const txb = new Transaction();
@@ -48,7 +49,6 @@ export const publishPackages = async (network: Network, isCiJob = false, configP
 			results[key] = data;
 
 			console.info(`Published ${key} with packageId: ${data.packageId}`);
-
 			writeFileSync(manifestFile, pkg.manifest(data.packageId)); // update the manifest with the published-at field.
 		}
 	}
@@ -69,11 +69,11 @@ export const publishPackages = async (network: Network, isCiJob = false, configP
 					v1: data.SuiNS.packageId,
 				},
 				suinsObjectId: data.SuiNS.suins,
-				utilsPackageId: data.Utils.packageId,
+				// utilsPackageId: data.Utils.packageId,
 				registrationPackageId: data.Registration.packageId,
-				renewalPackageId: data.Renewal.packageId,
 				subNamesPackageId: data.Subdomains.packageId,
 				tempSubNamesProxyPackageId: data.TempSubdomainProxy.packageId,
+				paymentsPackageId: data.Payments.packageId,
 			},
 			null,
 			2,

scripts/transactions/coupons_upgrade.ts
@@ -1,0 +1,40 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { execSync } from 'child_process';
+import { writeFileSync } from 'fs';
+
+import { mainPackage, Network } from '../config/constants';
+
+const network = (process.env.NETWORK as Network) || 'mainnet';
+
+// Active env of sui has to be the same with the env we're publishing to.
+// if upgradeCap & gasObject is on mainnet, it has to be on mainnet.
+// Github actions are always on mainnet.
+const couponsUpgrade = async () => {
+	const gasObjectId = process.env.GAS_OBJECT;
+
+	// Enabling the gas Object check only on mainnet, to allow testnet multisig tests.
+	if (!gasObjectId) throw new Error('No gas object supplied for a mainnet transaction');
+
+	const currentDir = process.cwd();
+	const couponsDir = `${currentDir}/../packages/coupons`;
+	const txFilePath = `${currentDir}/tx/tx-data.txt`;
+	const upgradeCall = `sui client upgrade --upgrade-capability ${mainPackage[network].coupons.upgradeCap} --gas-budget 2000000000 --gas ${gasObjectId} --skip-dependency-verification --serialize-unsigned-transaction`;
+
+	try {
+		// Execute the command with the specified working directory and capture the output
+		const output = execSync(upgradeCall, { cwd: couponsDir, stdio: 'pipe' }).toString();
+
+		writeFileSync(txFilePath, output);
+		console.log('Upgrade transaction successfully created and saved to tx-data.txt');
+	} catch (error: any) {
+		console.error('Error during protocol upgrade:', error.message);
+		console.error('stderr:', error.stderr?.toString());
+		console.error('stdout:', error.stdout?.toString());
+		console.error('Command:', error.cmd);
+		process.exit(1); // Exit with an error code
+	}
+};
+
+couponsUpgrade();

iotaledger/iota-names

scripts/coupons/couponTransactions.ts
@@ -1,0 +1,35 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Transaction, TransactionObjectArgument } from '@iota/iota-sdk/transactions';
+
+import { mainPackage, Network } from '../config/constants';
+import { signAndExecute } from '../utils/utils';
+import { CouponType } from './coupon';
+
+const network = (process.env.NETWORK as Network) || 'testnet';
+const config = mainPackage[network];
+
+export const createCoupon = () => {
+	const tx = new Transaction();
+	const couponConfig = new CouponType(100, 0); // type = 0 for percentage discount, 15% off
+	// couponConfig.setName('fiveplus15percentoff');
+	couponConfig.setName('100percentoff');
+	couponConfig.setLengthRule([5, 63]);
+	couponConfig.setAvailableClaims(100);
+	couponConfig.setYears([1, 3]);
+	couponConfig.setExpiration('1800000000000');
+	couponConfig.toTransaction(tx, config);
+
+	return signAndExecute(tx, network);
+};
+
+export const applyCoupon =
+	(intent: TransactionObjectArgument, couponCode: string) => (tx: Transaction) => {
+		return tx.moveCall({
+			target: `${config.coupons.packageId}::coupon_house::apply_coupon`,
+			arguments: [tx.object(config.iotaNames), intent, tx.pure.string(couponCode), tx.object.clock()],
+		});
+	};
+
+// createCoupon();

scripts/discounts/discounts.ts
@@ -1,0 +1,74 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Transaction, TransactionObjectArgument } from '@iota/iota-sdk/transactions';
+
+import { mainPackage, Network } from '../config/constants';
+import { setupFreeClaimsForType } from '../config/discounts';
+import { newPriceConfig } from '../init/authorization';
+import { getObjectType, signAndExecute } from '../utils/utils';
+
+const network = (process.env.NETWORK as Network) || 'testnet';
+const config = mainPackage[network];
+
+export const authorizeDiscountType = (type: string) => {
+	const tx = new Transaction();
+	tx.moveCall({
+		target: `${config.discountsPackage.packageId}::discounts::authorize_type`,
+		arguments: [
+			tx.object(config.discountsPackage.discountHouseId),
+			tx.object(config.adminCap),
+			newPriceConfig({
+				txb: tx,
+				packageId: config.packageId,
+				ranges: [
+					[3, 3],
+					[4, 4],
+					[5, 63],
+				],
+				prices: [30, 30, 30], // Discount percentages
+			}),
+		],
+		typeArguments: [type],
+	});
+
+	return signAndExecute(tx, network);
+};
+
+export const applyDiscount = async (
+	intent: TransactionObjectArgument,
+	discountNft: string,
+	network: Network,
+	tx: Transaction,
+) => {
+	const discountNftType = await getObjectType(network, discountNft);
+
+	tx.moveCall({
+		target: `${config.discountsPackage.packageId}::discounts::apply_percentage_discount`,
+		arguments: [
+			tx.object(config.discountsPackage.discountHouseId),
+			intent,
+			tx.object(config.iotaNames),
+			tx.object(discountNft),
+		],
+		typeArguments: [discountNftType],
+	});
+};
+
+// authorizeDiscountType(
+// 	'0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::iotafrens::SuiFren<0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::capy::Capy>',
+// );
+
+const setupFreeClaim = () => {
+	const tx = new Transaction();
+	setupFreeClaimsForType(
+		tx,
+		config,
+		'0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::iotafrens::SuiFren<0x80d7de9c4a56194087e0ba0bf59492aa8e6a5ee881606226930827085ddf2332::capy::Capy>',
+		{ from: 4, to: 5 },
+	);
+
+	return signAndExecute(tx, network);
+};
+
+// setupFreeClaim();

scripts/init/authorization.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // Modifications Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
-
-import { TransactionArgument, type Transaction } from '@iota/iota-sdk/transactions';
+import { Transaction, TransactionArgument } from '@iota/iota-sdk/transactions';
+import { hexToBytes } from '@noble/hashes/utils';
 
 /**
  * A helper to authorize any app in the IotaNames object.
@@ -81,17 +81,17 @@ export const addConfig = ({
 	iotaNames,
 	type,
 	config,
-	iotaNamesPackageIdV1,
+	iotaNamesPackageId,
 }: {
 	txb: Transaction;
 	adminCap: string;
 	iotaNames: string;
-	iotaNamesPackageIdV1: string;
+	iotaNamesPackageId: string;
 	config: TransactionArgument;
 	type: string;
 }) => {
 	txb.moveCall({
-		target: `${iotaNamesPackageIdV1}::iota_names::add_config`,
+		target: `${iotaNamesPackageId}::iota_names::add_config`,
 		arguments: [txb.object(adminCap), txb.object(iotaNames), config],
 		typeArguments: [type],
 	});
@@ -123,7 +123,7 @@ export const removeConfig = ({
 /**
  * Creates a default `config` which saves the price list and public key.
  */
-export const newPriceConfig = ({
+export const newPriceConfigV1 = ({
 	txb,
 	iotaNamesPackageIdV1,
 	priceList,
@@ -139,6 +139,173 @@ export const newPriceConfig = ({
 			txb.pure.u64(priceList.four),
 			txb.pure.u64(priceList.fivePlus),
 		],
+	});
+};
+
+export const newPriceConfig = ({
+	txb,
+	packageId,
+	ranges,
+	prices,
+}: {
+	txb: Transaction;
+	packageId: string;
+	ranges: number[][];
+	prices: number[];
+}): TransactionArgument => {
+	var rangesList: TransactionArgument[] = [];
+	for (const range of ranges) {
+		if (range.length !== 2) {
+			throw new Error('Each range must have exactly 2 elements');
+		}
+		rangesList.push(newRange({ txb, packageId, range }));
+	}
+	return txb.moveCall({
+		target: `${packageId}::pricing_config::new`,
+		arguments: [
+			txb.makeMoveVec({ elements: rangesList, type: `${packageId}::pricing_config::Range` }),
+			txb.pure.vector('u64', prices),
+		],
+	});
+};
+
+export const addCoreConfig = ({
+	txb,
+	latestPackageId,
+}: {
+	txb: Transaction;
+	latestPackageId: string;
+}) => {
+	return txb.moveCall({
+		target: `${latestPackageId}::core_config::new`,
+		arguments: [
+			txb.pure.vector('string', []),
+			txb.pure.u8(3),
+			txb.pure.u8(63),
+			txb.pure.u8(1),
+			txb.pure.u8(5),
+			txb.pure.vector('string', ['iota']),
+			txb.moveCall({
+				target: '0x2::vec_map::empty',
+				typeArguments: ['0x1::string::String', '0x1::string::String'],
+			}),
+		],
+	});
+};
+
+export const newRenewalConfig = ({
+	txb,
+	packageId,
+	ranges,
+	prices,
+}: {
+	txb: Transaction;
+	packageId: string;
+	ranges: number[][];
+	prices: number[];
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: `${packageId}::pricing_config::new_renewal_config`,
+		arguments: [newPriceConfig({ txb, packageId, ranges, prices })],
+	});
+};
+
+export const newRange = ({
+	txb,
+	packageId,
+	range,
+}: {
+	txb: Transaction;
+	packageId: string;
+	range: number[];
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: `${packageId}::pricing_config::new_range`,
+		arguments: [txb.pure.vector('u64', range)],
+	});
+};
+
+export const newPaymentsConfig = ({
+	txb,
+	packageId,
+	coinTypeAndDiscount,
+	baseCurrencyType,
+	maxAge,
+}: {
+	txb: Transaction;
+	packageId: string;
+	coinTypeAndDiscount: [Record<string, string>, number][]; // Array of [{type: string, metadataId: string, feed: string}, discountPercentage] pairs
+	baseCurrencyType: string;
+	maxAge: number;
+}): TransactionArgument => {
+	const coinTypeDataList: TransactionArgument[] = [];
+
+	for (const [coin, discountPercentage] of coinTypeAndDiscount) {
+		coinTypeDataList.push(
+			newCoinTypeData({
+				txb,
+				packageId,
+				discountPercentage,
+				coinType: coin['type'],
+				coinMetadataId: coin['metadataId'],
+				priceFeed: coin['feed']
+					? new Uint8Array(
+							hexToBytes(coin['feed'].startsWith('0x') ? coin['feed'].slice(2) : coin['feed']),
+						)
+					: new Uint8Array(),
+			}),
+		);
+	}
+
+	return txb.moveCall({
+		target: `${packageId}::payments::new_payments_config`,
+		arguments: [
+			txb.makeMoveVec({
+				elements: coinTypeDataList,
+				type: `${packageId}::payments::CoinTypeData`,
+			}),
+			getTypeName({ txb, coinType: baseCurrencyType }),
+			txb.pure.u64(maxAge),
+		],
+	});
+};
+
+export const getTypeName = ({
+	txb,
+	coinType,
+}: {
+	txb: Transaction;
+	coinType: string;
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: '0x1::type_name::get',
+		typeArguments: [coinType],
+	});
+};
+
+export const newCoinTypeData = ({
+	txb,
+	packageId,
+	discountPercentage,
+	coinType,
+	coinMetadataId,
+	priceFeed,
+}: {
+	txb: Transaction;
+	packageId: string;
+	discountPercentage: number;
+	coinType: string;
+	coinMetadataId: string;
+	priceFeed: Uint8Array;
+}): TransactionArgument => {
+	return txb.moveCall({
+		target: `${packageId}::payments::new_coin_type_data`,
+		arguments: [
+			txb.object(coinMetadataId),
+			txb.pure.u8(discountPercentage),
+			txb.pure.vector('u8', priceFeed),
+		],
+		typeArguments: [coinType],
 	});
 };
 

scripts/init/publish.ts
@@ -18,11 +18,13 @@ export const publishPackages = async (network: Network, isCiJob = false, configP
 	// split by ordering, and publish in batch.
 	const orderings = [...new Set([...Object.values(packages).map((x) => x.order)])];
 
-	// We do the publishing in batches, because some
+	// We do the publishing in batches, because some needs to be published before others
 	for (const ordering of orderings) {
 		const list = Object.entries(packages).filter((x) => x[1].order === ordering);
 
 		for (const [key, pkg] of list) {
+			console.log(`Publishing ${key}...`);
+			console.log(`Package folder: ${pkg.folder}`);
 			const packageFolder = path.resolve(contractsPath, pkg.folder);
 			// remove the lockfile on CI to allow fresh flows.
 			if (isCiJob) {
@@ -65,14 +67,8 @@ export const publishPackages = async (network: Network, isCiJob = false, configP
 		path.resolve(path.resolve(__dirname, '../'), 'constants.sdk.json'),
 		JSON.stringify(
 			{
-				iotaNamesPackageId: {
-					latest: data.IotaNames.packageId,
-					v1: data.IotaNames.packageId,
-				},
+				iotaNamesPackageId: data.IotaNames.packageId,
 				iotaNamesObjectId: data.IotaNames.iotaNames,
-				utilsPackageId: data.Utils.packageId,
-				registrationPackageId: data.Registration.packageId,
-				renewalPackageId: data.Renewal.packageId,
 				subNamesPackageId: data.Subdomains.packageId,
 				tempSubNamesProxyPackageId: data.TempSubdomainProxy.packageId,
 			},

scripts/transactions/coupons_upgrade.ts
@@ -1,0 +1,40 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { execSync } from 'child_process';
+import { writeFileSync } from 'fs';
+
+import { mainPackage, Network } from '../config/constants';
+
+const network = (process.env.NETWORK as Network) || 'mainnet';
+
+// Active env of iota has to be the same with the env we're publishing to.
+// if upgradeCap & gasObject is on mainnet, it has to be on mainnet.
+// Github actions are always on mainnet.
+const couponsUpgrade = async () => {
+	const gasObjectId = process.env.GAS_OBJECT;
+
+	// Enabling the gas Object check only on mainnet, to allow testnet multisig tests.
+	if (!gasObjectId) throw new Error('No gas object supplied for a mainnet transaction');
+
+	const currentDir = process.cwd();
+	const couponsDir = `${currentDir}/../packages/coupons`;
+	const txFilePath = `${currentDir}/tx/tx-data.txt`;
+	const upgradeCall = `iota client upgrade --upgrade-capability ${mainPackage[network].coupons.upgradeCap} --gas-budget 2000000000 --gas ${gasObjectId} --skip-dependency-verification --serialize-unsigned-transaction`;
+
+	try {
+		// Execute the command with the specified working directory and capture the output
+		const output = execSync(upgradeCall, { cwd: couponsDir, stdio: 'pipe' }).toString();
+
+		writeFileSync(txFilePath, output);
+		console.log('Upgrade transaction successfully created and saved to tx-data.txt');
+	} catch (error: any) {
+		console.error('Error during protocol upgrade:', error.message);
+		console.error('stderr:', error.stderr?.toString());
+		console.error('stdout:', error.stdout?.toString());
+		console.error('Command:', error.cmd);
+		process.exit(1); // Exit with an error code
+	}
+};
+
+couponsUpgrade();