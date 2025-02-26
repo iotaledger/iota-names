MystenLabs/suins-contracts

Upgrade SuiNS (#242)
* upgrade action

* suins packages

scripts/transactions/main_package_upgrade.ts
@@ -2,29 +2,39 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { execSync } from 'child_process';
-import dotenv from 'dotenv';
+import { writeFileSync } from 'fs';
 
 import { mainPackage, Network } from '../config/constants';
 
-dotenv.config();
-
-const gasObject = process.env.GAS_OBJECT;
 const network = (process.env.NETWORK as Network) || 'mainnet';
 
 // Active env of sui has to be the same with the env we're publishing to.
 // if upgradeCap & gasObject is on mainnet, it has to be on mainnet.
 // Github actions are always on mainnet.
 const mainPackageUpgrade = async () => {
-	if (!gasObject) throw new Error('Gas Object not supplied for a mainnet transaction');
+	const gasObjectId = process.env.GAS_OBJECT;
 
-	// on GH Action, the sui binary is located on root. Referencing that as `/` doesn't work.
-	const suiFolder = process.env.ORIGIN === 'gh_action' ? '../../sui' : 'sui';
-	const upgradeCall = `${suiFolder} client upgrade --upgrade-capability ${mainPackage[network].upgradeCap} --gas-budget 3000000000 --gas ${gasObject} --serialize-unsigned-transaction`;
+	// Enabling the gas Object check only on mainnet, to allow testnet multisig tests.
+	if (!gasObjectId) throw new Error('No gas object supplied for a mainnet transaction');
 
-	// we execute this on `setup/package.json` so we go one level back, access packages folder -> suins -> upgrade.
-	// we go from scripts/(base)/packages/suins, we run the upgrade and then we save the transaction data
-	// to suins/..(packages)/..(base)/scripts/tx/tx-data.txt
-	execSync(`cd $PWD/../packages/suins && ${upgradeCall} > $PWD/../../scripts/tx/tx-data.txt`);
+	const currentDir = process.cwd();
+	const suinsDir = `${currentDir}/../packages/suins`;
+	const txFilePath = `${currentDir}/tx/tx-data.txt`;
+	const upgradeCall = `sui client upgrade --upgrade-capability ${mainPackage[network].upgradeCap} --gas-budget 2000000000 --gas ${gasObjectId} --skip-dependency-verification --serialize-unsigned-transaction`;
+
+	try {
+		// Execute the command with the specified working directory and capture the output
+		const output = execSync(upgradeCall, { cwd: suinsDir, stdio: 'pipe' }).toString();
+
+		writeFileSync(txFilePath, output);
+		console.log('Upgrade transaction successfully created and saved to tx-data.txt');
+	} catch (error: any) {
+		console.error('Error during protocol upgrade:', error.message);
+		console.error('stderr:', error.stderr?.toString());
+		console.error('stdout:', error.stdout?.toString());
+		console.error('Command:', error.cmd);
+		process.exit(1); // Exit with an error code
+	}
 };
 
 mainPackageUpgrade();

iotaledger/iota-names

scripts/transactions/main_package_upgrade.ts
@@ -3,29 +3,39 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { execSync } from 'child_process';
-import dotenv from 'dotenv';
+import { writeFileSync } from 'fs';
 
 import { mainPackage, Network } from '../config/constants';
 
-dotenv.config();
-
-const gasObject = process.env.GAS_OBJECT;
 const network = (process.env.NETWORK as Network) || 'mainnet';
 
 // Active env of iota has to be the same with the env we're publishing to.
 // if upgradeCap & gasObject is on mainnet, it has to be on mainnet.
 // Github actions are always on mainnet.
 const mainPackageUpgrade = async () => {
-	if (!gasObject) throw new Error('Gas Object not supplied for a mainnet transaction');
+	const gasObjectId = process.env.GAS_OBJECT;
 
-	// on GH Action, the iota binary is located on root. Referencing that as `/` doesn't work.
-	const iotaFolder = process.env.ORIGIN === 'gh_action' ? '../../iota' : 'iota';
-	const upgradeCall = `${iotaFolder} client upgrade --upgrade-capability ${mainPackage[network].upgradeCap} --gas-budget 3000000000 --gas ${gasObject} --serialize-unsigned-transaction`;
+	// Enabling the gas Object check only on mainnet, to allow testnet multisig tests.
+	if (!gasObjectId) throw new Error('No gas object supplied for a mainnet transaction');
 
-	// we execute this on `setup/package.json` so we go one level back, access packages folder -> iota-names -> upgrade.
-	// we go from scripts/(base)/packages/iota-names, we run the upgrade and then we save the transaction data
-	// to iota-names/..(packages)/..(base)/scripts/tx/tx-data.txt
-	execSync(`cd $PWD/../packages/iota-names && ${upgradeCall} > $PWD/../../scripts/tx/tx-data.txt`);
+	const currentDir = process.cwd();
+	const iotaNamesDir = `${currentDir}/../packages/iota-names`;
+	const txFilePath = `${currentDir}/tx/tx-data.txt`;
+	const upgradeCall = `iota client upgrade --upgrade-capability ${mainPackage[network].upgradeCap} --gas-budget 2000000000 --gas ${gasObjectId} --skip-dependency-verification --serialize-unsigned-transaction`;
+
+	try {
+		// Execute the command with the specified working directory and capture the output
+		const output = execSync(upgradeCall, { cwd: iotaNamesDir, stdio: 'pipe' }).toString();
+
+		writeFileSync(txFilePath, output);
+		console.log('Upgrade transaction successfully created and saved to tx-data.txt');
+	} catch (error: any) {
+		console.error('Error during protocol upgrade:', error.message);
+		console.error('stderr:', error.stderr?.toString());
+		console.error('stdout:', error.stdout?.toString());
+		console.error('Command:', error.cmd);
+		process.exit(1); // Exit with an error code
+	}
 };
 
 mainPackageUpgrade();