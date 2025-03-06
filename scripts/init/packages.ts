MystenLabs/suins-contracts

SDK, Payments Tests, Package Management (#241)
* sdk changes

* update using new free claims

* lockfile update

* action update'

* authorize free claims

* new package constants

* lockfile updates

* upgrade payment dependency

* new move packages

* actions for authorizing packages

* authorize package action

* cleanup

* sdk

* changeset

* deauthorize old packages

scripts/init/packages.ts
@@ -3,13 +3,16 @@
 
 import { SuiTransactionBlockResponse } from '@mysten/sui/client';
 import { Transaction } from '@mysten/sui/transactions';
-import { MIST_PER_SUI } from '@mysten/sui/utils';
 
+import { Config, mainPackage, MAX_AGE, MIST_PER_USDC } from '../config/constants';
 import {
 	addConfig,
 	addRegistry,
 	newLookupRegistry,
-	newPriceConfig,
+	newPaymentsConfig,
+	newPriceConfigV1,
+	newPriceConfigV2,
+	newRenewalConfig,
 	setupApp,
 } from './authorization';
 import { createDisplay } from './display_tp';
@@ -31,6 +34,13 @@ const parseCorePackageObjects = (data: SuiTransactionBlockResponse) => {
 const parseCreatedObject = (data: SuiTransactionBlockResponse, objectType: string) => {
 	const obj = data.objectChanges!.find((x) => x.type === 'created' && x.objectType === objectType);
 	if (!obj || obj.type !== 'created') throw new Error(`Expected ${objectType} object`);
+
+	return obj.objectId;
+};
+
+const parseMutatedObject = (data: SuiTransactionBlockResponse, objectType: string) => {
+	const obj = data.objectChanges!.find((x) => x.type === 'mutated' && x.objectType === objectType);
+	if (!obj || obj.type !== 'mutated') throw new Error(`Expected ${objectType} object`);
 
 	return obj.objectId;
 };
@@ -80,16 +90,42 @@ export const Packages = (network: Network) => {
 					adminCap,
 					suins,
 					suinsPackageIdV1: packageId,
-					config: newPriceConfig({
+					config: newPriceConfigV2({
 						txb,
-						suinsPackageIdV1: packageId,
-						priceList: {
-							three: 5 * Number(MIST_PER_SUI),
-							four: 2 * Number(MIST_PER_SUI),
-							fivePlus: 0.5 * Number(MIST_PER_SUI),
-						},
+						packageId,
+						ranges: [
+							[3, 3],
+							[4, 4],
+							[5, 63],
+						],
+						prices: [
+							500 * Number(MIST_PER_USDC),
+							100 * Number(MIST_PER_USDC),
+							10 * Number(MIST_PER_USDC),
+						],
 					}),
-					type: `${packageId}::config::Config`,
+					type: `${packageId}::pricing_config::PricingConfig`,
+				});
+				addConfig({
+					txb,
+					adminCap,
+					suins,
+					suinsPackageIdV1: packageId,
+					config: newRenewalConfig({
+						txb,
+						packageId,
+						ranges: [
+							[3, 3],
+							[4, 4],
+							[5, 63],
+						],
+						prices: [
+							150 * Number(MIST_PER_USDC),
+							50 * Number(MIST_PER_USDC),
+							5 * Number(MIST_PER_USDC),
+						],
+					}),
+					type: `${packageId}::pricing_config::RenewalConfig`,
 				});
 				// create display for names
 				createDisplay({
@@ -110,6 +146,7 @@ export const Packages = (network: Network) => {
 					subdomainsPackageId: packageId,
 				});
 			},
+			authorizationType: (packageId: string) => `${packageId}::controller::Controller`, // Authorize the suins controller
 		},
 		Utils: {
 			order: 2,
@@ -140,62 +177,6 @@ export const Packages = (network: Network) => {
 			authorizationType: (packageId: string) => `${packageId}::denylist::DenyListAuth`,
 			setupFunction: (txb: Transaction, packageId: string, adminCap: string, suins: string) => {
 				setupApp({ txb, adminCap, suins, target: `${packageId}::denylist` });
-			},
-		},
-		Registration: {
-			order: 2,
-			folder: 'registration',
-			manifest: SuiNSDependentPackages(rev, 'registration'),
-			processPublish: (data: SuiTransactionBlockResponse) => {
-				const { packageId, upgradeCap } = parseCorePackageObjects(data);
-
-				return {
-					packageId,
-					upgradeCap,
-				};
-			},
-			authorizationType: (packageId: string) => `${packageId}::register::Register`,
-		},
-		Renewal: {
-			order: 2,
-			folder: 'renewal',
-			manifest: SuiNSDependentPackages(rev, 'renewal'),
-			processPublish: (data: SuiTransactionBlockResponse) => {
-				const { packageId, upgradeCap } = parseCorePackageObjects(data);
-
-				return {
-					packageId,
-					upgradeCap,
-				};
-			},
-			authorizationType: (packageId: string) => `${packageId}::renew::Renew`,
-			setupFunction: ({
-				txb,
-				packageId,
-				adminCap,
-				suinsPackageIdV1,
-				suins,
-				priceList,
-			}: {
-				txb: Transaction;
-				packageId: string;
-				suinsPackageIdV1: string;
-				adminCap: string;
-				suins: string;
-				priceList: { [key: string]: number };
-			}) => {
-				const configuration = newPriceConfig({
-					txb,
-					suinsPackageIdV1,
-					priceList,
-				});
-				setupApp({
-					txb,
-					adminCap,
-					suins: suins,
-					target: `${packageId}::renew::setup`,
-					args: [configuration],
-				});
 			},
 		},
 		DayOne: {
@@ -237,6 +218,54 @@ export const Packages = (network: Network) => {
 				suins: string;
 			}) => {
 				setupApp({ txb, adminCap, suins, target: `${packageId}::coupon_house` });
+			},
+		},
+		Payments: {
+			order: 2,
+			folder: 'payments',
+			manifest: SuiNSDependentPackages(rev, 'payments'),
+			processPublish: (data: SuiTransactionBlockResponse) => {
+				const { packageId, upgradeCap } = parseCorePackageObjects(data);
+
+				return {
+					packageId,
+					upgradeCap,
+				};
+			},
+			authorizationType: (packageId: string) => `${packageId}::payments::PaymentsApp`,
+			setupFunction: ({
+				txb,
+				packageId,
+				adminCap,
+				suins,
+				suinsPackageIdV1,
+			}: {
+				txb: Transaction;
+				packageId: string;
+				adminCap: string;
+				suins: string;
+				suinsPackageIdV1: string;
+			}) => {
+				const config = mainPackage[network as keyof Config];
+				const paymentsconfig = newPaymentsConfig({
+					txb,
+					packageId,
+					coinTypeAndDiscount: [
+						[config.coins.USDC, 0],
+						[config.coins.SUI, 0],
+						[config.coins.NS, 25],
+					],
+					baseCurrencyType: config.coins.USDC.type,
+					maxAge: MAX_AGE,
+				});
+				addConfig({
+					txb,
+					adminCap,
+					suins,
+					suinsPackageIdV1,
+					config: paymentsconfig,
+					type: `${packageId}::payments::PaymentsConfig`,
+				});
 			},
 		},
 		Subdomains: {
@@ -285,7 +314,7 @@ export const Packages = (network: Network) => {
 					discountHouse,
 				};
 			},
-			authorizationType: (packageId: string) => `${packageId}::house::DiscountHouseApp`,
+			authorizationType: (packageId: string) => `${packageId}::discounts::RegularDiscountsApp`,
 		},
 		TempSubdomainProxy: {
 			order: 3,

iotaledger/iota-names

scripts/init/packages.ts
@@ -6,11 +6,14 @@ import { IotaTransactionBlockResponse } from '@iota/iota-sdk/client';
 import { Transaction } from '@iota/iota-sdk/transactions';
 import { NANOS_PER_IOTA } from '@iota/iota-sdk/utils';
 
+import { Config, mainPackage, MAX_AGE } from '../config/constants';
 import {
 	addConfig,
 	addRegistry,
 	newLookupRegistry,
+	newPaymentsConfig,
 	newPriceConfig,
+	newRenewalConfig,
 	setupApp,
 } from './authorization';
 import { createDisplay } from './display_tp';
@@ -31,6 +34,13 @@ const parseCorePackageObjects = (data: IotaTransactionBlockResponse) => {
 const parseCreatedObject = (data: IotaTransactionBlockResponse, objectType: string) => {
 	const obj = data.objectChanges!.find((x) => x.type === 'created' && x.objectType === objectType);
 	if (!obj || obj.type !== 'created') throw new Error(`Expected ${objectType} object`);
+
+	return obj.objectId;
+};
+
+const parseMutatedObject = (data: IotaTransactionBlockResponse, objectType: string) => {
+	const obj = data.objectChanges!.find((x) => x.type === 'mutated' && x.objectType === objectType);
+	if (!obj || obj.type !== 'mutated') throw new Error(`Expected ${objectType} object`);
 
 	return obj.objectId;
 };
@@ -66,8 +76,8 @@ export const Packages = (network: Network) => {
 					txb,
 					adminCap,
 					iotaNames,
-					iotaNamesPackageIdV1: packageId,
-					registry: newLookupRegistry({ txb, iotaNamesPackageIdV1: packageId, adminCap: adminCap }),
+					iotaNamesPackageId: packageId,
+					registry: newLookupRegistry({ txb, iotaNamesPackageId: packageId, adminCap: adminCap }),
 					type: `${packageId}::registry::Registry`,
 				});
 				// Adds the configuration file (pricelist and public key)
@@ -75,24 +85,50 @@ export const Packages = (network: Network) => {
 					txb,
 					adminCap,
 					iotaNames,
-					iotaNamesPackageIdV1: packageId,
+					iotaNamesPackageId: packageId,
 					config: newPriceConfig({
 						txb,
-						iotaNamesPackageIdV1: packageId,
-						priceList: {
-							three: 5 * Number(NANOS_PER_IOTA),
-							four: 2 * Number(NANOS_PER_IOTA),
-							fivePlus: 0.5 * Number(NANOS_PER_IOTA),
-						},
+						packageId,
+						ranges: [
+							[3, 3],
+							[4, 4],
+							[5, 63],
+						],
+						prices: [
+							500 * Number(NANOS_PER_IOTA),
+							100 * Number(NANOS_PER_IOTA),
+							10 * Number(NANOS_PER_IOTA),
+						],
 					}),
-					type: `${packageId}::config::Config`,
+					type: `${packageId}::pricing_config::PricingConfig`,
+				});
+				addConfig({
+					txb,
+					adminCap,
+					iotaNames,
+					iotaNamesPackageId: packageId,
+					config: newRenewalConfig({
+						txb,
+						packageId,
+						ranges: [
+							[3, 3],
+							[4, 4],
+							[5, 63],
+						],
+						prices: [
+							150 * Number(NANOS_PER_IOTA),
+							50 * Number(NANOS_PER_IOTA),
+							5 * Number(NANOS_PER_IOTA),
+						],
+					}),
+					type: `${packageId}::pricing_config::RenewalConfig`,
 				});
 				// create display for names
 				createDisplay({
 					txb,
 					publisher,
 					isSubdomain: false,
-					iotaNamesPackageIdV1: packageId,
+					iotaNamesPackageId: packageId,
 					network: 'testnet',
 					subdomainsPackageId: packageId,
 				});
@@ -101,24 +137,12 @@ export const Packages = (network: Network) => {
 					txb,
 					publisher,
 					isSubdomain: true,
-					iotaNamesPackageIdV1: packageId,
+					iotaNamesPackageId: packageId,
 					network: 'testnet',
 					subdomainsPackageId: packageId,
 				});
 			},
-		},
-		Utils: {
-			order: 2,
-			folder: 'utils',
-			processPublish: (data: IotaTransactionBlockResponse) => {
-				const { packageId, upgradeCap } = parseCorePackageObjects(data);
-
-				return {
-					packageId,
-					upgradeCap,
-				};
-			},
-			authorizationType: (packageId: string) => `${packageId}::direct_setup::DirectSetup`,
+			authorizationType: (packageId: string) => `${packageId}::controller::Controller`, // Authorize the iotaNames controller
 		},
 		DenyList: {
 			order: 2,
@@ -136,9 +160,9 @@ export const Packages = (network: Network) => {
 				setupApp({ txb, adminCap, iotaNames, target: `${packageId}::denylist` });
 			},
 		},
-		Registration: {
+		Payments: {
 			order: 2,
-			folder: 'registration',
+			folder: 'payments',
 			processPublish: (data: IotaTransactionBlockResponse) => {
 				const { packageId, upgradeCap } = parseCorePackageObjects(data);
 
@@ -147,46 +171,39 @@ export const Packages = (network: Network) => {
 					upgradeCap,
 				};
 			},
-			authorizationType: (packageId: string) => `${packageId}::register::Register`,
-		},
-		Renewal: {
-			order: 2,
-			folder: 'renewal',
-			processPublish: (data: IotaTransactionBlockResponse) => {
-				const { packageId, upgradeCap } = parseCorePackageObjects(data);
-
-				return {
-					packageId,
-					upgradeCap,
-				};
-			},
-			authorizationType: (packageId: string) => `${packageId}::renew::Renew`,
+			authorizationType: (packageId: string) => `${packageId}::payments::PaymentsApp`,
 			setupFunction: ({
 				txb,
 				packageId,
 				adminCap,
-				iotaNamesPackageIdV1,
 				iotaNames,
-				priceList,
+				iotaNamesPackageId,
 			}: {
 				txb: Transaction;
 				packageId: string;
-				iotaNamesPackageIdV1: string;
 				adminCap: string;
 				iotaNames: string;
-				priceList: { [key: string]: number };
+				iotaNamesPackageId: string;
 			}) => {
-				const configuration = newPriceConfig({
+				const config = mainPackage[network as keyof Config];
+				const paymentsconfig = newPaymentsConfig({
 					txb,
-					iotaNamesPackageIdV1,
-					priceList,
+					packageId,
+					coinTypeAndDiscount: [
+						[config.coins.USDC, 0],
+						[config.coins.IOTA, 0],
+						[config.coins.NS, 25],
+					],
+					baseCurrencyType: config.coins.USDC.type,
+					maxAge: MAX_AGE,
 				});
-				setupApp({
+				addConfig({
 					txb,
 					adminCap,
-					iotaNames: iotaNames,
-					target: `${packageId}::renew::setup`,
-					args: [configuration],
+					iotaNames,
+					iotaNamesPackageId,
+					config: paymentsconfig,
+					type: `${packageId}::payments::PaymentsConfig`,
 				});
 			},
 		},
@@ -206,13 +223,13 @@ export const Packages = (network: Network) => {
 				packageId: string,
 				adminCap: string,
 				iotaNames: string,
-				iotaNamesPackageIdV1: string,
+				iotaNamesPackageId: string,
 			) => {
 				addConfig({
 					txb,
 					adminCap,
 					iotaNames,
-					iotaNamesPackageIdV1,
+					iotaNamesPackageId,
 					config: txb.moveCall({
 						target: `${packageId}::config::default`,
 					}),