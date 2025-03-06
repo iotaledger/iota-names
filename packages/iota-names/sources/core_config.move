MystenLabs/suins-contracts

[Payments V2] Core Revamp (#210)
Package refactoring to support payments v2 and clean up our packages.

packages/suins/sources/core_config.move
@@ -1,0 +1,111 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+/// Core configuration of the SuiNS application.
+///
+/// This configuration is used to validate domains for registration and renewal.
+/// It can only be stored as a valid config in the `SuiNS` object by an admin,
+/// hence why all the functions are public. Having just the config object cannot
+/// pose a security risk as it cannot be used.
+module suins::core_config;
+
+use std::string::String;
+use sui::vec_map::{Self, VecMap};
+use sui::vec_set::{Self, VecSet};
+use suins::constants;
+use suins::domain::Domain;
+
+#[error]
+const EInvalidLength: vector<u8> = b"Invalid length for the label part of the domain.";
+
+#[error]
+const EInvalidTld: vector<u8> = b"Invalid TLD";
+
+#[error]
+const ESubnameNotSupported: vector<u8> = b"Subdomains are not supported for sales.";
+
+public struct CoreConfig has copy, drop, store {
+    /// Public key of the API server. Currently only used for direct setup.
+    public_key: vector<u8>,
+    /// Minimum length of the label part of the domain. This is different from
+    /// the base `domain` checks. This is our minimum acceptable length (for sales).
+    min_label_length: u8,
+    /// Maximum length of the label part of the domain.
+    max_label_length: u8,
+    /// List of valid TLDs for registration / renewals.
+    valid_tlds: VecSet<String>,
+    /// The `PaymentIntent` version that can be used for handling sales.
+    payments_version: u8,
+    /// Maximum number of years available for a domain.
+    max_years: u8,
+    // Extra fields for future use.
+    extra: VecMap<String, String>,
+}
+
+public fun new(
+    public_key: vector<u8>,
+    min_label_length: u8,
+    max_label_length: u8,
+    payments_version: u8,
+    max_years: u8,
+    valid_tlds: vector<String>,
+    extra: VecMap<String, String>,
+): CoreConfig {
+    CoreConfig {
+        public_key,
+        min_label_length,
+        max_label_length,
+        payments_version,
+        max_years,
+        valid_tlds: vec_set::from_keys(valid_tlds),
+        extra,
+    }
+}
+
+public fun public_key(config: &CoreConfig): vector<u8> {
+    config.public_key
+}
+
+public fun min_label_length(config: &CoreConfig): u8 {
+    config.min_label_length
+}
+
+public fun max_label_length(config: &CoreConfig): u8 {
+    config.max_label_length
+}
+
+public fun is_valid_tld(config: &CoreConfig, tld: &String): bool {
+    config.valid_tlds.contains(tld)
+}
+
+public fun payments_version(config: &CoreConfig): u8 {
+    config.payments_version
+}
+
+public fun max_years(config: &CoreConfig): u8 {
+    config.max_years
+}
+
+public(package) fun assert_is_valid_for_sale(config: &CoreConfig, domain: &Domain) {
+    assert!(!domain.is_subdomain(), ESubnameNotSupported);
+    assert!(config.is_valid_tld(domain.tld()), EInvalidTld);
+
+    let sld_len = domain.sld().length();
+    assert!(
+        sld_len >= (config.min_label_length as u64) && sld_len <= (config.max_label_length as u64),
+        EInvalidLength,
+    );
+}
+
+#[test_only]
+public fun default(): CoreConfig {
+    new(
+        b"",
+        3,
+        63,
+        constants::payments_version!(),
+        5,
+        vector[constants::sui_tld()],
+        vec_map::empty(),
+    )
+}

packages/suins/sources/sales/pricing_config.move
@@ -1,0 +1,91 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module suins::pricing_config;
+
+use sui::vec_map::{Self, VecMap};
+
+/// Tries to create a range with more than 2 values
+const EInvalidLength: u64 = 1;
+/// Tries to create a range with the first value greater than the second
+const EInvalidRange: u64 = 2;
+/// Tries to create a pricing config with different lengths for ranges and
+/// prices
+const ELengthMissmatch: u64 = 3;
+/// Tries to calculate the price for a given length
+const EPriceNotSet: u64 = 4;
+
+/// A range struct that holds the start and end of a range (inclusive).
+public struct Range(u64, u64) has copy, drop, store;
+
+/// A struct that holds the length range and the price of a service.
+public struct PricingConfig has drop, store { pricing: VecMap<Range, u64> }
+
+/// A struct that holds the renewal configuration. Exposed from base pricing
+/// module
+/// to allow easy access to the pricing config by external packages.
+public struct RenewalConfig has drop, store { config: PricingConfig }
+
+/// Calculates the base price for a given length.
+/// - Base price type is abstracted away. We can switch to a different base.
+///  Our core base will become USDC.
+/// - The price is calculated based on the length of the domain name and the
+/// available ranges.
+public fun calculate_base_price(config: &PricingConfig, length: u64): u64 {
+    let keys = config.pricing.keys();
+    let mut idx = keys.find_index!(|range| range.is_between_inclusive(length));
+
+    assert!(idx.is_some(), EPriceNotSet);
+    let range = keys[idx.extract()];
+
+    *config.pricing.get(&range)
+}
+
+/// Creates a new PricingConfig with the given ranges and prices.
+/// - The ranges should be sorted in `ascending order` and should not overlap.
+/// - The length of the ranges and prices should be the same.
+///
+/// All the ranges are inclusive (e.g. [3,5]: includes 3, 4, and 5).
+public fun new(ranges: vector<Range>, prices: vector<u64>): PricingConfig {
+    assert!(ranges.length() == prices.length(), ELengthMissmatch);
+    // Validate that our ranges are passed in the correct order
+    // we expect them to be sorted in ascending order, and we expect them
+    // to not have any overlaps.
+    let mut i = 1;
+
+    while (i < ranges.length()) {
+        assert!(ranges[i - 1].1 < ranges[i].0, EInvalidRange);
+        i = i + 1;
+    };
+
+    // let sorted = ranges.
+    PricingConfig {
+        pricing: vec_map::from_keys_values(ranges, prices),
+    }
+}
+
+/// Checks if the value is between the range (inclusive).
+public fun is_between_inclusive(range: &Range, length: u64): bool {
+    length >= range.0 && length <= range.1
+}
+
+/// Returns the pricing config for usage in external apps.
+public fun pricing(config: &PricingConfig): &VecMap<Range, u64> {
+    &config.pricing
+}
+
+/// Constructor for Renewal<T> that initializes it with a PricingConfig.
+public fun new_renewal_config(config: PricingConfig): RenewalConfig {
+    RenewalConfig { config }
+}
+
+public fun new_range(range: vector<u64>): Range {
+    assert!(range.length() == 2, EInvalidLength);
+    assert!(range[0] <= range[1], EInvalidRange);
+
+    Range(range[0], range[1])
+}
+
+public fun config(renewal: &RenewalConfig): &PricingConfig {
+    &renewal.config
+}

packages/suins/tests/payments/pricing_tests.move
@@ -1,0 +1,107 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module suins::pricing_config_tests;
+
+use suins::pricing_config;
+
+#[test]
+fun test_e2e() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[11, 20]),
+        pricing_config::new_range(vector[21, 30]),
+        pricing_config::new_range(vector[31, 31]),
+    ];
+
+    let pricing_config = pricing_config::new(
+        ranges,
+        vector[10, 20, 30, 45]
+    );
+
+    // test internal values
+    assert!(pricing_config.calculate_base_price(5) == 10);
+    assert!(pricing_config.calculate_base_price(15) == 20);
+    assert!(pricing_config.calculate_base_price(25) == 30);
+
+    // test upper bounds
+    assert!(pricing_config.calculate_base_price(10) == 10);
+    assert!(pricing_config.calculate_base_price(20) == 20);
+    assert!(pricing_config.calculate_base_price(30) == 30);
+
+    // test lower bounds
+    assert!(pricing_config.calculate_base_price(1) == 10);
+    assert!(pricing_config.calculate_base_price(11) == 20);
+    assert!(pricing_config.calculate_base_price(21) == 30);
+
+    // single length pricing
+    assert!(pricing_config.calculate_base_price(31) == 45);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::EInvalidRange)]
+fun test_range_overlap_1() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[9, 20]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20]);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::EInvalidRange)]
+fun test_range_overlap_2() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[10, 20]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20]);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::EInvalidRange)]
+fun test_range_overlap_3() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[21, 30]),
+        pricing_config::new_range(vector[11, 20]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20, 30]);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::EInvalidRange)]
+fun test_range_overlap_4() {
+    let ranges = vector[
+        pricing_config::new_range(vector[20, 30]),
+        pricing_config::new_range(vector[30, 40]),
+        pricing_config::new_range(vector[40, 50]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20, 30]);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::ELengthMissmatch)]
+fun test_length_missmatch() {
+    let ranges = vector[pricing_config::new_range(vector[10, 20])];
+
+    pricing_config::new(ranges, vector[10, 20]);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::EInvalidLength)]
+fun test_range_construction_too_long() {
+    pricing_config::new_range(vector[10, 20, 30]);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::EInvalidRange)]
+fun test_invalid_range_construction() {
+    pricing_config::new_range(vector[20, 10]);
+}
+
+#[test, expected_failure(abort_code = ::suins::pricing_config::EPriceNotSet)]
+fun test_price_not_set() {
+    let ranges = vector[pricing_config::new_range(vector[1, 10])];
+
+    let pricing = pricing_config::new(ranges, vector[10]);
+
+    pricing.calculate_base_price(20);
+}

packages/suins/tests/unit/core_config_tests.move
@@ -1,0 +1,89 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module suins::core_config_tests;
+
+use sui::test_utils::assert_eq;
+use sui::vec_map;
+use suins::constants;
+use suins::core_config::{Self, CoreConfig};
+use suins::domain;
+
+#[test]
+fun test_config_creation_and_field_access() {
+    let config = core_config::new(
+        b"",
+        3,
+        63,
+        constants::payments_version!(),
+        1,
+        vector[constants::sui_tld()],
+        vec_map::empty(),
+    );
+
+    assert_eq(config.public_key(), b"");
+    assert_eq(config.min_label_length(), 3);
+    assert_eq(config.max_label_length(), 63);
+    assert_eq(config.payments_version(), constants::payments_version!());
+    assert!(config.is_valid_tld(&constants::sui_tld()));
+}
+
+#[test]
+fun test_valid_domains() {
+    let config = core_config::default();
+    let mut domain = domain::new(b"suins.sui".to_string());
+    config.assert_is_valid_for_sale(&domain);
+
+    domain = domain::new(b"sui.sui".to_string());
+    config.assert_is_valid_for_sale(&domain);
+}
+
+#[test]
+fun custom_config_valid_length() {
+    let config = core_config::new(
+        b"",
+        1,
+        63,
+        constants::payments_version!(),
+        5,
+        vector[constants::sui_tld()],
+        vec_map::empty(),
+    );
+    config.assert_is_valid_for_sale(&domain::new(b"0.sui".to_string()));
+}
+
+#[test, expected_failure(abort_code = core_config::EInvalidTld)]
+fun test_invalid_tld() {
+    core_config::default().assert_is_valid_for_sale(&domain::new(b"suins.move".to_string()));
+}
+
+#[test, expected_failure(abort_code = core_config::EInvalidLength)]
+fun test_invalid_label_length() {
+    core_config::default().assert_is_valid_for_sale(&domain::new(b"o.sui".to_string()));
+}
+
+#[test, expected_failure(abort_code = core_config::EInvalidLength)]
+fun test_invalid_label_length_2() {
+    custom_config(1, 5).assert_is_valid_for_sale(
+        &domain::new(b"123456.sui".to_string()),
+    );
+}
+
+#[test, expected_failure(abort_code = core_config::ESubnameNotSupported)]
+fun test_subname_not_supported() {
+    custom_config(1, 5).assert_is_valid_for_sale(
+        &domain::new(b"inner.suins.sui".to_string()),
+    );
+}
+
+fun custom_config(min: u8, max: u8): CoreConfig {
+    core_config::new(
+        b"",
+        min,
+        max,
+        constants::payments_version!(),
+        5,
+        vector[constants::sui_tld()],
+        vec_map::empty(),
+    )
+}

iotaledger/iota-names

packages/iota-names/sources/core_config.move
@@ -1,0 +1,110 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+/// Core configuration of the iota_names application.
+///
+/// This configuration is used to validate domains for registration and renewal.
+/// It can only be stored as a valid config in the `iota_names` object by an admin,
+/// hence why all the functions are public. Having just the config object cannot
+/// pose a security risk as it cannot be used.
+module iota_names::core_config;
+
+use std::string::String;
+use iota::vec_map::VecMap;
+use iota::vec_set::{Self, VecSet};
+use iota_names::domain::Domain;
+
+#[error]
+const EInvalidLength: vector<u8> = b"Invalid length for the label part of the domain.";
+
+#[error]
+const EInvalidTld: vector<u8> = b"Invalid TLD";
+
+#[error]
+const ESubnameNotSupported: vector<u8> = b"Subdomains are not supported for sales.";
+
+public struct CoreConfig has copy, drop, store {
+    /// Public key of the API server. Currently only used for direct setup.
+    public_key: vector<u8>,
+    /// Minimum length of the label part of the domain. This is different from
+    /// the base `domain` checks. This is our minimum acceptable length (for sales).
+    min_label_length: u8,
+    /// Maximum length of the label part of the domain.
+    max_label_length: u8,
+    /// List of valid TLDs for registration / renewals.
+    valid_tlds: VecSet<String>,
+    /// The `PaymentIntent` version that can be used for handling sales.
+    payments_version: u8,
+    /// Maximum number of years available for a domain.
+    max_years: u8,
+    // Extra fields for future use.
+    extra: VecMap<String, String>,
+}
+
+public fun new(
+    public_key: vector<u8>,
+    min_label_length: u8,
+    max_label_length: u8,
+    payments_version: u8,
+    max_years: u8,
+    valid_tlds: vector<String>,
+    extra: VecMap<String, String>,
+): CoreConfig {
+    CoreConfig {
+        public_key,
+        min_label_length,
+        max_label_length,
+        payments_version,
+        max_years,
+        valid_tlds: vec_set::from_keys(valid_tlds),
+        extra,
+    }
+}
+
+public fun public_key(config: &CoreConfig): vector<u8> {
+    config.public_key
+}
+
+public fun min_label_length(config: &CoreConfig): u8 {
+    config.min_label_length
+}
+
+public fun max_label_length(config: &CoreConfig): u8 {
+    config.max_label_length
+}
+
+public fun is_valid_tld(config: &CoreConfig, tld: &String): bool {
+    config.valid_tlds.contains(tld)
+}
+
+public fun payments_version(config: &CoreConfig): u8 {
+    config.payments_version
+}
+
+public fun max_years(config: &CoreConfig): u8 {
+    config.max_years
+}
+
+public(package) fun assert_is_valid_for_sale(config: &CoreConfig, domain: &Domain) {
+    assert!(!domain.is_subdomain(), ESubnameNotSupported);
+    assert!(config.is_valid_tld(domain.tld()), EInvalidTld);
+
+    let sld_len = domain.sld().length();
+    assert!(
+        sld_len >= (config.min_label_length as u64) && sld_len <= (config.max_label_length as u64),
+        EInvalidLength,
+    );
+}
+
+#[test_only]
+public fun default(): CoreConfig {
+    new(
+        b"",
+        3,
+        63,
+        iota_names::constants::payments_version!(),
+        5,
+        vector[iota_names::constants::iota_tld()],
+        iota::vec_map::empty(),
+    )
+}

packages/iota-names/sources/sales/pricing_config.move
@@ -1,0 +1,91 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module iota_names::pricing_config;
+
+use iota::vec_map::{Self, VecMap};
+
+/// Tries to create a range with more than 2 values
+const EInvalidLength: u64 = 1;
+/// Tries to create a range with the first value greater than the second
+const EInvalidRange: u64 = 2;
+/// Tries to create a pricing config with different lengths for ranges and
+/// prices
+const ELengthMissmatch: u64 = 3;
+/// Tries to calculate the price for a given length
+const EPriceNotSet: u64 = 4;
+
+/// A range struct that holds the start and end of a range (inclusive).
+public struct Range(u64, u64) has copy, drop, store;
+
+/// A struct that holds the length range and the price of a service.
+public struct PricingConfig has drop, store { pricing: VecMap<Range, u64> }
+
+/// A struct that holds the renewal configuration. Exposed from base pricing
+/// module
+/// to allow easy access to the pricing config by external packages.
+public struct RenewalConfig has drop, store { config: PricingConfig }
+
+/// Calculates the base price for a given length.
+/// - Base price type is abstracted away. We can switch to a different base.
+///  Our core base will become USDC.
+/// - The price is calculated based on the length of the domain name and the
+/// available ranges.
+public fun calculate_base_price(config: &PricingConfig, length: u64): u64 {
+    let keys = config.pricing.keys();
+    let mut idx = keys.find_index!(|range| range.is_between_inclusive(length));
+
+    assert!(idx.is_some(), EPriceNotSet);
+    let range = keys[idx.extract()];
+
+    *config.pricing.get(&range)
+}
+
+/// Creates a new PricingConfig with the given ranges and prices.
+/// - The ranges should be sorted in `ascending order` and should not overlap.
+/// - The length of the ranges and prices should be the same.
+///
+/// All the ranges are inclusive (e.g. [3,5]: includes 3, 4, and 5).
+public fun new(ranges: vector<Range>, prices: vector<u64>): PricingConfig {
+    assert!(ranges.length() == prices.length(), ELengthMissmatch);
+    // Validate that our ranges are passed in the correct order
+    // we expect them to be sorted in ascending order, and we expect them
+    // to not have any overlaps.
+    let mut i = 1;
+
+    while (i < ranges.length()) {
+        assert!(ranges[i - 1].1 < ranges[i].0, EInvalidRange);
+        i = i + 1;
+    };
+
+    // let sorted = ranges.
+    PricingConfig {
+        pricing: vec_map::from_keys_values(ranges, prices),
+    }
+}
+
+/// Checks if the value is between the range (inclusive).
+public fun is_between_inclusive(range: &Range, length: u64): bool {
+    length >= range.0 && length <= range.1
+}
+
+/// Returns the pricing config for usage in external apps.
+public fun pricing(config: &PricingConfig): &VecMap<Range, u64> {
+    &config.pricing
+}
+
+/// Constructor for Renewal<T> that initializes it with a PricingConfig.
+public fun new_renewal_config(config: PricingConfig): RenewalConfig {
+    RenewalConfig { config }
+}
+
+public fun new_range(range: vector<u64>): Range {
+    assert!(range.length() == 2, EInvalidLength);
+    assert!(range[0] <= range[1], EInvalidRange);
+
+    Range(range[0], range[1])
+}
+
+public fun config(renewal: &RenewalConfig): &PricingConfig {
+    &renewal.config
+}

packages/iota-names/tests/payments/pricing_tests.move
@@ -1,0 +1,107 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module iota_names::pricing_config_tests;
+
+use iota_names::pricing_config;
+
+#[test]
+fun test_e2e() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[11, 20]),
+        pricing_config::new_range(vector[21, 30]),
+        pricing_config::new_range(vector[31, 31]),
+    ];
+
+    let pricing_config = pricing_config::new(
+        ranges,
+        vector[10, 20, 30, 45]
+    );
+
+    // test internal values
+    assert!(pricing_config.calculate_base_price(5) == 10);
+    assert!(pricing_config.calculate_base_price(15) == 20);
+    assert!(pricing_config.calculate_base_price(25) == 30);
+
+    // test upper bounds
+    assert!(pricing_config.calculate_base_price(10) == 10);
+    assert!(pricing_config.calculate_base_price(20) == 20);
+    assert!(pricing_config.calculate_base_price(30) == 30);
+
+    // test lower bounds
+    assert!(pricing_config.calculate_base_price(1) == 10);
+    assert!(pricing_config.calculate_base_price(11) == 20);
+    assert!(pricing_config.calculate_base_price(21) == 30);
+
+    // single length pricing
+    assert!(pricing_config.calculate_base_price(31) == 45);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::EInvalidRange)]
+fun test_range_overlap_1() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[9, 20]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20]);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::EInvalidRange)]
+fun test_range_overlap_2() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[10, 20]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20]);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::EInvalidRange)]
+fun test_range_overlap_3() {
+    let ranges = vector[
+        pricing_config::new_range(vector[1, 10]),
+        pricing_config::new_range(vector[21, 30]),
+        pricing_config::new_range(vector[11, 20]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20, 30]);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::EInvalidRange)]
+fun test_range_overlap_4() {
+    let ranges = vector[
+        pricing_config::new_range(vector[20, 30]),
+        pricing_config::new_range(vector[30, 40]),
+        pricing_config::new_range(vector[40, 50]),
+    ];
+
+    pricing_config::new(ranges, vector[10, 20, 30]);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::ELengthMissmatch)]
+fun test_length_missmatch() {
+    let ranges = vector[pricing_config::new_range(vector[10, 20])];
+
+    pricing_config::new(ranges, vector[10, 20]);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::EInvalidLength)]
+fun test_range_construction_too_long() {
+    pricing_config::new_range(vector[10, 20, 30]);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::EInvalidRange)]
+fun test_invalid_range_construction() {
+    pricing_config::new_range(vector[20, 10]);
+}
+
+#[test, expected_failure(abort_code = ::iota_names::pricing_config::EPriceNotSet)]
+fun test_price_not_set() {
+    let ranges = vector[pricing_config::new_range(vector[1, 10])];
+
+    let pricing = pricing_config::new(ranges, vector[10]);
+
+    pricing.calculate_base_price(20);
+}

packages/iota-names/tests/unit/core_config_tests.move
@@ -1,0 +1,89 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module iota_names::core_config_tests;
+
+use iota::test_utils::assert_eq;
+use iota::vec_map;
+use iota_names::constants;
+use iota_names::core_config::{Self, CoreConfig};
+use iota_names::domain;
+
+#[test]
+fun test_config_creation_and_field_access() {
+    let config = core_config::new(
+        b"",
+        3,
+        63,
+        constants::payments_version!(),
+        1,
+        vector[constants::iota_tld()],
+        vec_map::empty(),
+    );
+
+    assert_eq(config.public_key(), b"");
+    assert_eq(config.min_label_length(), 3);
+    assert_eq(config.max_label_length(), 63);
+    assert_eq(config.payments_version(), constants::payments_version!());
+    assert!(config.is_valid_tld(&constants::iota_tld()));
+}
+
+#[test]
+fun test_valid_domains() {
+    let config = core_config::default();
+    let mut domain = domain::new(b"iota_names.iota".to_string());
+    config.assert_is_valid_for_sale(&domain);
+
+    domain = domain::new(b"iota.iota".to_string());
+    config.assert_is_valid_for_sale(&domain);
+}
+
+#[test]
+fun custom_config_valid_length() {
+    let config = core_config::new(
+        b"",
+        1,
+        63,
+        constants::payments_version!(),
+        5,
+        vector[constants::iota_tld()],
+        vec_map::empty(),
+    );
+    config.assert_is_valid_for_sale(&domain::new(b"0.iota".to_string()));
+}
+
+#[test, expected_failure(abort_code = core_config::EInvalidTld)]
+fun test_invalid_tld() {
+    core_config::default().assert_is_valid_for_sale(&domain::new(b"iota_names.move".to_string()));
+}
+
+#[test, expected_failure(abort_code = core_config::EInvalidLength)]
+fun test_invalid_label_length() {
+    core_config::default().assert_is_valid_for_sale(&domain::new(b"o.iota".to_string()));
+}
+
+#[test, expected_failure(abort_code = core_config::EInvalidLength)]
+fun test_invalid_label_length_2() {
+    custom_config(1, 5).assert_is_valid_for_sale(
+        &domain::new(b"123456.iota".to_string()),
+    );
+}
+
+#[test, expected_failure(abort_code = core_config::ESubnameNotSupported)]
+fun test_subname_not_supported() {
+    custom_config(1, 5).assert_is_valid_for_sale(
+        &domain::new(b"inner.iota_names.iota".to_string()),
+    );
+}
+
+fun custom_config(min: u8, max: u8): CoreConfig {
+    core_config::new(
+        b"",
+        min,
+        max,
+        constants::payments_version!(),
+        5,
+        vector[constants::iota_tld()],
+        vec_map::empty(),
+    )
+}