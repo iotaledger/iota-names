e5dece857c8adce537d3d6e8129ccc52d5e26205
/alex/ <alexander.schmidt@iota.org>
2025-04-02 17:09:14 +0200

    lint:fix

--- scripts/init/init.ts
+++ scripts/init/init.ts
@@ -3,52 +3,57 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { IotaObjectResponse } from '@iota/iota-sdk/client';
-import { getActiveAddress, getClient, signAndExecute } from '../utils/utils';
 import { Transaction } from '@iota/iota-sdk/transactions';
+
+import { getActiveAddress, getClient, signAndExecute } from '../utils/utils';
 import { publishPackages } from './publish';
 import { setup } from './setup';
 
 // Extract network argument from command-line arguments
 const args = process.argv.slice(2); // Get arguments passed after `node init.ts`
 if (args.length !== 2) {
-    throw new Error(
-        'Invalid number of arguments. Please be sure to provide exactly TWO arguments for the network.',
-    );
+	throw new Error(
+		'Invalid number of arguments. Please be sure to provide exactly TWO arguments for the network.',
+	);
 }
 
 const newOwner = args[1]; // Second argument should be the address of the new newOwner
 
-export const init = async (network: string | undefined, newOwner: string | undefined, isCIJob: boolean) => {
-    if (!network)
-        throw new Error(
-            'Network not defined. Please run `export NETWORK=mainnet|testnet|devnet|localnet`',
-        );
+export const init = async (
+	network: string | undefined,
+	newOwner: string | undefined,
+	isCIJob: boolean,
+) => {
+	if (!network)
+		throw new Error(
+			'Network not defined. Please run `export NETWORK=mainnet|testnet|devnet|localnet`',
+		);
 
-    if (!newOwner)
-        throw new Error(
-            'Owner not defined. Please provide the new newOwner address of IOTA-Names, e.g. a multisig address',
-        );
+	if (!newOwner)
+		throw new Error(
+			'Owner not defined. Please provide the new newOwner address of IOTA-Names, e.g. a multisig address',
+		);
 
-    const published = await publishPackages(network, isCIJob, process.env.CLIENT_CONFIG_FILE);
-    console.log('Published:', published);
-    await setup(published, network);
+	const published = await publishPackages(network, isCIJob, process.env.CLIENT_CONFIG_FILE);
+	console.log('Published:', published);
+	await setup(published, network);
 
-    const client = getClient(network);
-    const res = await client.getOwnedObjects({
-        newOwner: getActiveAddress(),
-        options: { showType: true },
-    });
-    const ownedNonCoinObjects = (res.data as IotaObjectResponse[])
-        .filter((x) => x.data?.type !== '0x2::coin::Coin<0x2::iota::IOTA>')
-        .map((x) => x.data?.objectId)
-        .filter((id): id is string => typeof id === "string");
+	const client = getClient(network);
+	const res = await client.getOwnedObjects({
+		newOwner: getActiveAddress(),
+		options: { showType: true },
+	});
+	const ownedNonCoinObjects = (res.data as IotaObjectResponse[])
+		.filter((x) => x.data?.type !== '0x2::coin::Coin<0x2::iota::IOTA>')
+		.map((x) => x.data?.objectId)
+		.filter((id): id is string => typeof id === 'string');
 
-    const tx = new Transaction();
-    tx.transferObjects(ownedNonCoinObjects, newOwner);
+	const tx = new Transaction();
+	tx.transferObjects(ownedNonCoinObjects, newOwner);
 
-    const result = await signAndExecute(tx, network);
-    const response = await client.waitForTransaction({ digest: result.digest });
-    console.log(`Transaction digest: ${response.digest}`);
+	const result = await signAndExecute(tx, network);
+	const response = await client.waitForTransaction({ digest: result.digest });
+	console.log(`Transaction digest: ${response.digest}`);
 };
 
 init(process.env.NETWORK, newOwner, !!process.env.IS_CI_JOB);
