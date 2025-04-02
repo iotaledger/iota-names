2a143a3619f7a3d3097faf6332503287bda476a9
Antonino <ant.iaria@stud.uniroma3.it>
2025-04-02 16:05:58 +0200

    fix: placed args length check before calling init

--- scripts/init/init.ts
+++ scripts/init/init.ts
@@ -7,12 +7,18 @@ import { setup } from './setup';
 
 // Extract network argument from command-line arguments
 const args = process.argv.slice(2); // Get arguments passed after `node init.ts`
+if (args.length !== 1) {
+	throw new Error(
+		'Invalid number of arguments. Please be sure to provide exactly ONE argument for the network.',
+	);
+}
+
 const network = args[0]; // First argument should be the network
 
 export const init = async (network: string | undefined, isCIJob: boolean) => {
-	if (!network || args.length !== 1) {
+	if (!network) {
 		throw new Error(
-			'Network not defined or too many arguments. Please run `pnpm ts-node init.js <network>` (e.g., mainnet, testnet, devnet, localnet)\n Be sure to provide exactly ONE argument for the network.',
+			'Network not defined or too many arguments. Please run `pnpm ts-node init.js <network>` (e.g., mainnet, testnet, devnet, localnet)',
 		);
 	}
 
