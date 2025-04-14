06a8a86556de8a6eac8ebc9fdf3d9b1eb6d52905
Chloe Martin <chloedaughterofmars@gmail.com>
2025-04-14 10:00:55 +0200

    fixes

--- sdk/src/utils.ts
+++ sdk/src/utils.ts
@@ -2,10 +2,10 @@
 // Modifications Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-const LABEL_REGEX = /[a-z0-9-]{0,63}/i;
-const PATH_REGEX = new RegExp(`(?:(${LABEL_REGEX.source})(?:\\.(${LABEL_REGEX.source}))*)`, 'i');
-const NAME_REGEX = new RegExp(`^${PATH_REGEX.source}@(${LABEL_REGEX.source})$`, 'i');
-const DOMAIN_REGEX = new RegExp(`^(?![-.])(?:(${LABEL_REGEX.source})\\.)+iota$`, 'i');
+const LABEL_REGEX = /(?!-)[a-z0-9-]{0,62}[a-z0-9]/i;
+const PATH_REGEX = new RegExp(`(?:${LABEL_REGEX.source}(?:\\.${LABEL_REGEX.source})*)`, 'i');
+const NAME_REGEX = new RegExp(`^${PATH_REGEX.source}@${LABEL_REGEX.source}$`, 'i');
+const DOMAIN_REGEX = new RegExp(`^(?:${LABEL_REGEX.source}\\.)+(iota)$`, 'i');
 const MAX_LENGTH = 235;
 
 export function isValidIotaName(name: string): boolean {
@@ -18,15 +18,20 @@ export function isValidIotaName(name: string): boolean {
 
 export function normalizeIotaName(name: string, format: 'at' | 'dot' = 'at'): string {
 	const lowerCase = name.toLowerCase();
-	let parts = NAME_REGEX.exec(lowerCase) ?? DOMAIN_REGEX.exec(lowerCase);
+	let parts;
 
-	if (parts == null) {
+	if (NAME_REGEX.test(lowerCase)) {
+		let [path, domain] = lowerCase.split('@');
+		parts = [...path.split('.'), domain];
+	} else if (DOMAIN_REGEX.test(lowerCase)) {
+		parts = lowerCase.split('.');
+	} else {
 		throw new Error(`Invalid IOTA name ${name}`);
 	}
 
 	if (format === 'dot') {
 		return parts.join('.');
 	} else {
-		return `${parts.slice(0, -1).join('.')}@${parts[-1]}`;
+		return `${parts.slice(0, -1).join('.')}@${parts[parts.length - 1]}`;
 	}
 }
