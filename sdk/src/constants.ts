78be8ed1ee2f1443526523e41473ca5dace8e647
evavirseda <evirseda@boxfish.studio>
2025-05-07 11:23:09 +0200

    update devnet ids

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -16,23 +16,23 @@ export const ALLOWED_METADATA = {
 
 export const mainPackage: Config = {
 	devnet: {
-		auctionPackageId: '0xe24e72f0623ea19b4b2ec847e2b208033c6f7938b50e31efe4996aa2f23d4477',
-		packageId: '0xa1d2ed2008d31d358cfaf61a89aa7cfaa78ed183dbe683620258e98c59f48b13',
-		packageIdPricing: '0xa1d2ed2008d31d358cfaf61a89aa7cfaa78ed183dbe683620258e98c59f48b13',
-		iotaNames: '0xbf3563622035af599057c46f4b871e0a9817c7bab759354532402be6d9538ba3',
-		subNamesPackageId: '0xeaa8d89e19d6f346f7a515bb3cf63fd9b7b456e77a0073a13103bdfbdd8d1980',
+		auctionPackageId: '0x5e7a300e640f645a4030aeb507c7be16909e6fa9711e7ca2d4397bbd967d5c50',
+		packageId: '0x3ec4826f1d6e0d9f00680b2e9a7a41f03788ee610b3d11c24f41ab0ae71da39f',
+		packageIdPricing: '0x3ec4826f1d6e0d9f00680b2e9a7a41f03788ee610b3d11c24f41ab0ae71da39f',
+		iotaNames: '0x54a8a67fad7aa279429e08824e03481dd8b268779353d299d7f8edaa8b8c13b7',
+		subNamesPackageId: '0x2e541b250f53d45e9b4cb866be2ab3d8815015a249a094e63b196cc184278925',
 		tempSubdomainsProxyPackageId:
-			'0x11e01b25113cf141676d2f0b97068adbd2c98dd15ce1f52bd21c595faf63ec55',
+			'0x3f2927e8e78a094bb98271fd73e06a3aa056edfecbf32dddb8a65d30de9b8a3f',
 		payments: {
-			packageId: '0xb06b8075797480a9bb660c927b666ca0301cdffa622e7c6b9c583bd2b45c781a',
+			packageId: '0x882f88d252a650649f490e96e32e53979758fdda645863ca856d83c72d5e0e72',
 		},
 		coins: {
 			IOTA: {
 				type: '0x0000000000000000000000000000000000000000000000000000000000000002::iota::IOTA',
 			},
 		},
-		registryTableId: '0xd5e98aa3e79cff0cd5146dc4d7dea863eaffcce06703e47473f88214c4746501',
-		reverseRegistryTableId: '0xcafc893c3801416ffa4c262888eaa994e055d717d9b0819db3aef4ce35ab5829',
+		registryTableId: '0xef24c78e8c085e29760d37b287fc16647f0f578e8d22f18dd65f655285afad3e',
+		reverseRegistryTableId: '0x566dc13eafceaf8c3487ee2c41464553839ef4d50937c63741e359c98080c7b6',
 	},
 	mainnet: {
 		auctionPackageId: '',
