14057c844cc662a3a898c0559c9b57183650b396
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-05-28 18:30:15 +0200

    feat(dapp): modify sign and execute tx

--- dapp/src/components/PurchaseName.tsx
+++ dapp/src/components/PurchaseName.tsx
@@ -17,9 +17,12 @@ type PurchaseNameProps = {
 };
 
 export function PurchaseName({ domainName, onClose }: PurchaseNameProps) {
+    const iotaClient = useIotaClient();
     const { iotaNamesClient } = useIotaNamesClient();
     const { isConnected, currentWallet } = useCurrentWallet();
     const [price, setPrice] = useState<number | null>(null);
+    const [isExecuted, setIsExecuted] = useState(false);
+    const [digest, setDigest] = useState<string>();
 
     useEffect(() => {
         async function fetchPrice() {
@@ -36,70 +39,92 @@ export function PurchaseName({ domainName, onClose }: PurchaseNameProps) {
     }, [domainName, iotaNamesClient]);
 
     async function handlePurchase(domainName: string) {
-        // try {
-        if (!currentWallet) {
-            throw new Error('Wallet is not connected');
+        try {
+            if (!currentWallet) {
+                throw new Error('Wallet is not connected');
+            }
+            console.log('[Current wallet]: ', currentWallet);
+            const address = currentWallet.accounts[0].address;
+            if (!address) {
+                throw new Error('Wallet address is undefined');
+            }
+
+            const domainLabels = domainName.split('.');
+            if (domainName.split('.').length !== 2) {
+                throw new Error('Subdomains not supported yet');
+            }
+
+            const length = domainLabels[0].length;
+            if (length < 3) {
+                throw new Error('Name too short (minimum 3 characters)');
+            }
+
+            const domainRegistered = await iotaNamesClient.getNameRecord(domainName);
+            if (domainRegistered) {
+                throw new Error('Domain name already registered');
+            }
+
+            // Build transaction
+            const tx = new Transaction();
+            const iotaNamesTx = new IotaNamesTransaction(iotaNamesClient, tx);
+            if (price === null) {
+                throw new Error('Price is not available');
+            }
+            const [coin] = iotaNamesTx.transaction.splitCoins(tx.gas, [price]);
+            const nft = iotaNamesTx.register({
+                domain: domainName!,
+                years: 1,
+                coin,
+            });
+            iotaNamesTx.transaction.transferObjects([nft], address);
+            iotaNamesTx.transaction.transferObjects([coin], address);
+            iotaNamesTx.transaction.setSender(address);
+            const transaction = await iotaNamesTx.transaction.build({
+                client: iotaClient,
+            });
+
+            //const signer = getSigner();
+            // const executedTransactionResponse = await iotaClient.signAndExecuteTransaction({
+            //     transaction,
+            //     signer: signer,
+            //     options: {
+            //         showEffects: true,
+            //         showObjectChanges: true,
+            //     },
+            // });
+
+            const signAndExecuteTransaction =
+                currentWallet.features['iota:signAndExecuteTransaction']?.signAndExecuteTransaction;
+
+            if (!signAndExecuteTransaction) {
+                throw new Error(
+                    'signAndExecuteTransaction feature is not available in the current wallet',
+                );
+            }
+            const executedTransactionResponse = await signAndExecuteTransaction({
+                transaction: {
+                    toJSON: async () => JSON.stringify(transaction),
+                },
+                account: currentWallet.accounts[0],
+                chain: 'devnet:iota',
+            });
+            console.log('[Transaction digest]: ', executedTransactionResponse.digest);
+
+            await iotaClient.waitForTransaction({
+                digest: executedTransactionResponse.digest,
+            });
+            const transactionBlockResponse = await iotaClient.getTransactionBlock({
+                digest: executedTransactionResponse.digest,
+                options: {
+                    showEffects: true,
+                },
+            });
+            setDigest(executedTransactionResponse.digest);
+            setIsExecuted(true);
+            console.log('[Transaction block response]: ', transactionBlockResponse);
+        } catch (error: unknown) {
+            throw Error('Error purchasing domain name: ', error as Error);
         }
-        const iotaClient = useIotaClient();
-        const address = currentWallet.accounts[0].address;
-        if (!address) {
-            throw new Error('Wallet address is undefined');
-        }
-
-        const domainLabels = domainName.split('.');
-        if (domainName.split('.').length !== 2) {
-            throw new Error('Subdomains not supported yet');
-        }
-
-        const length = domainLabels[0].length;
-        if (length < 3) {
-            throw new Error('Name too short (minimum 3 characters)');
-        }
-
-        const domainRegistered = await iotaNamesClient.getNameRecord(domainName);
-        if (domainRegistered) {
-            throw new Error('Domain name already registered');
-        }
-
-        // Build transaction
-        const tx = new Transaction();
-        const iotaNamesTx = new IotaNamesTransaction(iotaNamesClient, tx);
-        if (price === null) {
-            throw new Error('Price is not available');
-        }
-        const [coin] = iotaNamesTx.transaction.splitCoins(tx.gas, [price]);
-        const nft = iotaNamesTx.register({
-            domain: domainName!,
-            years: 1,
-            coin,
-        });
-        iotaNamesTx.transaction.transferObjects([nft], address);
-        iotaNamesTx.transaction.transferObjects([coin], address);
-        iotaNamesTx.transaction.setSender(address);
-        const transaction = await iotaNamesTx.transaction.build({
-            client: iotaClient,
-        });
-        const executedTransactionResponse = await iotaClient.signAndExecuteTransaction({
-            transaction,
-            signer: currentWallet,
-        });
-
-        console.log('[Transaction digest]: ', executedTransactionResponse.digest);
-
-        await iotaClient.waitForTransaction({
-            digest: executedTransactionResponse.digest,
-        });
-
-        const transactionBlockResponse = await iotaClient.getTransactionBlock({
-            digest: executedTransactionResponse.digest,
-            options: {
-                showEffects: true,
-            },
-        });
-        console.log('[Transaction block response]: ', transactionBlockResponse);
-        // } catch (error: unknown) {
-        //     throw Error('Error purchasing domain name: ');
-        // }
     }
     return (
         <div className="flex flex-col items-center w-full space-y-4">
@@ -146,6 +171,14 @@ export function PurchaseName({ domainName, onClose }: PurchaseNameProps) {
                             />
                         </div>
                     </div>
+                    {isExecuted && (
+                        <div className="bg-gray-200/80 dark:bg-gray-700/80 p-4 rounded text-left">
+                            <div className="text-xs text-gray-600 dark:text-gray-300 mb-1">
+                                TX DIGEST
+                            </div>
+                            <div className="font-mono">{digest}</div>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
