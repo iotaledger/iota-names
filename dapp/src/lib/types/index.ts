e858c9a972fde26e4d4634354dba68662094e4ad
Begoña Alvarez <balvarez@boxfish.studio>
2025-05-29 12:17:20 +0200

    chore: cleanup debris

--- dapp/src/lib/types/index.ts
+++ dapp/src/lib/types/index.ts
@@ -1,11 +1,4 @@
-// Copyright (c) 2024 IOTA Stiftung
+// Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-export * from './renderComponent';
-export * from './balanceChange';
-export * from './objectChange';
-export * from './transactionSummary';
 export * from './gasSummary';
-export * from './transactionExecute';
-export * from './validators';
-export * from './schema';
