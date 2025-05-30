ba1ca3c53a6067688fe7aa211fb842d2075078eb
evavirseda <evirseda@boxfish.studio>
2025-05-30 09:50:36 +0200

    update header

--- dapp/src/lib/utils/getGasSummary.ts
+++ dapp/src/lib/utils/getGasSummary.ts
@@ -1,6 +1,6 @@
-// Copyright (c) Mysten Labs, Inc.
-// Modifications Copyright (c) 2025 IOTA Stiftung
+// Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+
 
 import type {
     DryRunTransactionBlockResponse,
