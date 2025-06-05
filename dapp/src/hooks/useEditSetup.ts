2530ccdbcdd1a4d9068320ea672aa0ac9856a919
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-06-05 13:12:38 +0200

    feat(dapp): add set permissions

---
+++ dapp/src/hooks/useEditSetup.ts
@@ -0,0 +1,62 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { useIotaClient } from '@iota/dapp-kit';
+import { IotaNamesTransaction } from '@iota/iota-names-sdk';
+import { Transaction } from '@iota/iota-sdk/transactions';
+import { useQuery } from '@tanstack/react-query';
+
+import { getGasSummary } from '@/lib/utils/getGasSummary';
+import { useIotaNamesClient } from '@/providers/contexts';
+
+export function useEditSetup(
+    address: string,
+    parentNft: string,
+    name: string,
+    allowChildCreation: boolean,
+    allowTimeExtension: boolean,
+) {
+    const client = useIotaClient();
+    const { iotaNamesClient } = useIotaNamesClient();
+
+    return useQuery({
+        // eslint-disable-next-line @tanstack/query/exhaustive-deps
+        queryKey: [
+            'edit-setup-transaction',
+            address,
+            parentNft,
+            name,
+            allowChildCreation,
+            allowTimeExtension,
+        ],
+        queryFn: async () => {
+            const tx = new Transaction();
+            const iotaNamesTx = new IotaNamesTransaction(iotaNamesClient, tx);
+            iotaNamesTx.editSetup({
+                parentNft: tx.object(parentNft),
+                name,
+                allowChildCreation,
+                allowTimeExtension,
+            });
+            iotaNamesTx.transaction.setSender(address);
+            const transaction = await iotaNamesTx.transaction.build({
+                client,
+            });
+            const txDryRun = await client.dryRunTransactionBlock({
+                transactionBlock: transaction,
+            });
+            return {
+                transaction: tx,
+                txDryRun,
+            };
+        },
+        enabled: !!address && !!parentNft && !!name && !!allowChildCreation && !!allowTimeExtension,
+        gcTime: 0,
+        select: ({ transaction, txDryRun }) => {
+            return {
+                transaction,
+                gasSummary: getGasSummary(txDryRun),
+            };
+        },
+    });
+}
