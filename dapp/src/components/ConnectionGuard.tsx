f827b5e3f0bfc5b81cac6958f5be9d49ea90efe7
evavirseda <evirseda@boxfish.studio>
2025-06-05 07:49:49 +0200

    redirect to home

--- dapp/src/components/ConnectionGuard.tsx
+++ dapp/src/components/ConnectionGuard.tsx
@@ -4,28 +4,11 @@
 'use client';
 
 import { LoadingIndicator } from '@iota/apps-ui-kit';
-import { useAutoConnectWallet, useCurrentWallet } from '@iota/dapp-kit';
-import { redirect, usePathname } from 'next/navigation';
-import { PropsWithChildren, useEffect } from 'react';
-
-import { CONNECT_ROUTE, MY_NAMES_ROUTE } from '@/lib/constants';
+import { useAutoConnectWallet } from '@iota/dapp-kit';
+import { PropsWithChildren } from 'react';
 
 export function ConnectionGuard({ children }: PropsWithChildren) {
-    const { isConnected, isDisconnected } = useCurrentWallet();
-
-    const pathname = usePathname();
     const autoConnect = useAutoConnectWallet();
-
-    useEffect(() => {
-        if (autoConnect !== 'attempted') return;
-        if (isConnected && pathname === CONNECT_ROUTE.path) {
-            // Redirect to home if on root ("/")
-            redirect(MY_NAMES_ROUTE.path);
-        } else if (isDisconnected && pathname !== CONNECT_ROUTE.path) {
-            // Redirect back to "/" if disconnected and trying to access another page
-            redirect(CONNECT_ROUTE.path);
-        }
-    }, [isConnected, isDisconnected, pathname, autoConnect]);
 
     if (autoConnect === 'idle') {
         return (
