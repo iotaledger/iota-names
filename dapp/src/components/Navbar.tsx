2eb17e0fea231490fd65df26d64211a7e51b4402
evavirseda <evirseda@boxfish.studio>
2025-06-05 11:08:37 +0200

    feat(dapp): add protected `/my-names` route (#257)
    * first steps
    
    * protected routes
    
    * fix format
    
    * redirect to home

---
+++ dapp/src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+'use client';
+
+import { ConnectButton, useCurrentWallet } from '@iota/dapp-kit';
+import Link from 'next/link';
+
+import { PROTECTED_ROUTES } from '@/lib/constants';
+
+import { ThemeSwitcher } from './ThemeSwitcher';
+
+export function Navbar() {
+    const { isConnected } = useCurrentWallet();
+    return (
+        <nav className="sticky top-0 ">
+            <div className="container flex justify-between p-2">
+                <h1 className="flex items-center text-headline-sm">IOTA NAMES</h1>
+                <div className="flex flex-row gap-x-sm items-center">
+                    {isConnected &&
+                        PROTECTED_ROUTES.map((route) => (
+                            <Link
+                                key={route.path}
+                                href={route.path}
+                                className="text-label-md hover:text-primary-20 hover:dark:text-primary-80"
+                                data-testid={`${route.id}-link`}
+                            >
+                                {route.title}
+                            </Link>
+                        ))}
+                    <div className="flex items-center space-x-2">
+                        <ThemeSwitcher />
+                        <ConnectButton connectText="Connect" />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- dapp/src/contexts/index.ts
+++ dapp/src/contexts/index.ts
@@ -1,1 +1,4 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 export * from './ThemeContext';
