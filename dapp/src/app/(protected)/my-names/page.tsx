c83c18da01924f298cc243664f4282ed3ea1cf8d
Branko <brankobosnic1@gmail.com>
2025-06-05 10:52:42 +0200

    fix: add regustration nfts hook

--- dapp/src/app/(protected)/my-names/page.tsx
+++ dapp/src/app/(protected)/my-names/page.tsx
@@ -4,24 +4,13 @@
 'use client';
 
 import { KeyValueInfo, Title } from '@iota/apps-ui-kit';
-import { useCurrentAccount, useIotaClientContext } from '@iota/dapp-kit';
-import { getIotaNamesRegistrationType, mainPackage } from '@iota/iota-names-sdk';
 
 import { AvailabilityCheck } from '@/components';
-import { useGetAllOwnedObjects } from '@/hooks/useGetAllOwnedObjects';
+import { useRegistrationNfts } from '@/hooks/useRegistrationNfts';
 
 function MyNamesPage(): JSX.Element {
-    const account = useCurrentAccount();
-    const address = account?.address ?? '';
+    const registrationNfts = useRegistrationNfts();
 
-    const { network: networkName } = useIotaClientContext();
-    const config = mainPackage[networkName as keyof typeof mainPackage];
-
-    const { data: namesRegistrationData } = useGetAllOwnedObjects(address, {
-        StructType: getIotaNamesRegistrationType(config.packageId),
-    });
-
-    console.log('namesRegistrationData Objects:', namesRegistrationData);
     return (
         <div className="flex flex-col w-full gap-y-lg items-center">
             <AvailabilityCheck />
@@ -29,10 +18,11 @@ function MyNamesPage(): JSX.Element {
                 <Title title="My names" testId="my-names-page" />
             </div>
             <div className="flex flex-col gap-x-sm items-center">
-                {namesRegistrationData?.map((nameRecord) => (
+                {registrationNfts?.map((nameRecord) => (
                     <KeyValueInfo
-                        keyText={nameRecord?.display?.data?.name ?? ''}
-                        value={nameRecord?.display?.data?.description ?? ''}
+                        key={nameRecord.name}
+                        keyText={nameRecord.name}
+                        value={nameRecord?.description ?? ''}
                         fullwidth
                     />
                 ))}
