4c84c99e5dc822b5859f83c4c87e6b9eca3ef0ac
Panteley <panteleymonchuk@gmail.com>
2025-06-16 16:43:57 +0300

    feat(dapp): show subdomains (#270)
    * feat(dapp): show subdomains
    
    * chore: polish
    
    * refactor: update subnames ui
    
    * refactor: show subnames as flat
    
    * chore: pr comment changes

--- dapp/src/hooks/useRegistrationNfts.ts
+++ dapp/src/hooks/useRegistrationNfts.ts
@@ -2,13 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useCurrentAccount } from '@iota/dapp-kit';
-import { getIotaNamesRegistrationType } from '@iota/iota-names-sdk';
+import {
+    getIotaNamesRegistrationType,
+    getIotaSubdomainRegistrationType,
+} from '@iota/iota-names-sdk';
 
 import { useIotaNamesClient } from '@/providers/contexts';
 
 import { useGetAllOwnedObjects } from './useGetAllOwnedObjects';
 
-interface RegistrationNft {
+export interface RegistrationNft {
     name: string;
     description?: string;
     image_url?: string;
@@ -16,16 +19,28 @@ interface RegistrationNft {
     project_url?: string;
 }
 
-export function useRegistrationNfts() {
-    const account = useCurrentAccount();
-    const address = account?.address ?? '';
+type RegistrationNftType = 'domain' | 'subdomain';
 
+export function useRegistrationNfts(type: RegistrationNftType = 'domain') {
+    const account = useCurrentAccount();
     const { iotaNamesClient } = useIotaNamesClient();
+    const address = account?.address ?? '';
     const packageId = iotaNamesClient.config.packageId;
 
-    const { data: namesRegistrationData } = useGetAllOwnedObjects(address, {
-        StructType: getIotaNamesRegistrationType(packageId),
-    });
+    const filter = (() => {
+        switch (type) {
+            case 'domain':
+                return {
+                    StructType: getIotaNamesRegistrationType(packageId),
+                };
+            case 'subdomain':
+                return {
+                    StructType: getIotaSubdomainRegistrationType(packageId),
+                };
+        }
+    })();
+
+    const { data: namesRegistrationData } = useGetAllOwnedObjects(address, filter);
 
     const registrationNfts: RegistrationNft[] =
         namesRegistrationData?.map((nameRecord) => {
