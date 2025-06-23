12cec543c1e10aa44a751f44e734e1f574e196e7
Thibault Martinez <thibault@iota.org>
2025-06-23 14:38:13 +0200

    TODO

--- indexer/src/config.rs
+++ indexer/src/config.rs
@@ -63,9 +63,12 @@ impl IotaNamesExtendedConfig {
 
     // Create a config based on the package and object ids published on devnet.
     pub fn devnet() -> Self {
-        const AUCTION_PACKAGE_ADDRESS: &str = "0x";
+        const AUCTION_PACKAGE_ADDRESS: &str =
+            "0x5e7a300e640f645a4030aeb507c7be16909e6fa9711e7ca2d4397bbd967d5c50";
+        // TODO https://github.com/iotaledger/iota-names/issues/318
         const COUPONS_PACKAGE_ADDRESS: &str = "0x";
-        const SUBDOMAINS_PACKAGE_ADDRESS: &str = "0x";
+        const SUBDOMAINS_PACKAGE_ADDRESS: &str =
+            "0x2e541b250f53d45e9b4cb866be2ab3d8815015a249a094e63b196cc184278925";
         const AUCTION_HOUSE_ID: &str =
             "0x31deb8cbd320867089d52c37fed2d443520aac0fc5a957de1f64f9135b83f42b";
 
