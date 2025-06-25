d09c2260d139aa6419b7b0edac401c99a2771681
marc2332 <mespinsanz@gmail.com>
2025-06-25 11:52:57 +0200

    clean up

--- dapp/src/lib/utils/names.ts
+++ dapp/src/lib/utils/names.ts
@@ -3,6 +3,51 @@
 
 import { NameRecord } from '@iota/iota-names-sdk';
 
+import { RegistrationNft } from '@/hooks';
+
 export function isNameRecordExpired(nameRecord: NameRecord) {
     return nameRecord.expirationTimestampMs < Date.now();
+}
+
+export function getNamePermissions(nameRecord: NameRecord) {
+    let allowChildCreation = false;
+    let allowTimeExtension = false;
+    if ('S_AC' in nameRecord.data) {
+        allowChildCreation = nameRecord.data.S_AC === '1';
+    }
+    if ('S_ATE' in nameRecord.data) {
+        allowTimeExtension = nameRecord.data.S_ATE === '1';
+    }
+
+    return {
+        allowChildCreation,
+        allowTimeExtension,
+    };
+}
+
+/**
+ * Get the parent object id of a given subndomain
+ */
+export function getParentSubdomainObjectId(
+    ownedNames: RegistrationNft[],
+    ownedSubdomains: RegistrationNft[],
+    name: string,
+) {
+    const parts = name.split('.');
+    const directParentName = parts.slice(1).join('.');
+    const parentParts = directParentName?.split('.').length;
+
+    if (parentParts === 2) {
+        // Subdomains with names as parent
+        const parentDomain = ownedNames.find(
+            (domain: { name: string | null }) => domain.name === directParentName,
+        );
+        return parentDomain?.id || null;
+    } else if (parentParts && parentParts >= 3) {
+        // Subdomains with another subdomain as parent
+        const parentSubdomain = ownedSubdomains.find(
+            (subdomain: { name: string | null }) => subdomain.name === directParentName,
+        );
+        return parentSubdomain?.id || null;
+    }
 }
