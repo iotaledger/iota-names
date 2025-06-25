fe1f3b4ff74175a27210a6bfc27bc2ab82e1c267
Keito <64607484+KeitoTadashi@users.noreply.github.com>
2025-06-25 13:21:06 +0200

    feat(dapp): add set permissions (#269)
    * feat(dapp): add set permissions
    
    * clean up
    
    * fmt
    
    * clean up
    
    ---------
    
    Co-authored-by: marc2332 <mespinsanz@gmail.com>

--- dapp/src/lib/utils/names.ts
+++ dapp/src/lib/utils/names.ts
@@ -3,6 +3,43 @@
 
 import { NameRecord } from '@iota/iota-names-sdk';
 
+import { RegistrationNft } from '@/hooks';
+
 export function isNameRecordExpired(nameRecord: NameRecord) {
     return nameRecord.expirationTimestampMs < Date.now();
+}
+
+export function getNamePermissions(nameRecord: NameRecord) {
+    let allowChildCreation = false;
+    let allowTimeExtension = false;
+    if ('S_AC' in nameRecord.data) {
+        allowChildCreation = nameRecord.data.S_AC === '1';
+    }
+    if ('S_ATE' in nameRecord.data) {
+        allowTimeExtension = nameRecord.data.S_ATE === '1';
+    }
+
+    return {
+        allowChildCreation,
+        allowTimeExtension,
+    };
+}
+
+/**
+ * Get the parent object id of a given subdomain
+ */
+export function getParentSubdomainObjectId(
+    ownedNames: RegistrationNft[],
+    ownedSubdomains: RegistrationNft[],
+    name: string,
+) {
+    const parts = name.split('.');
+    const parentName = parts.slice(1).join('.');
+    const parentParts = parentName?.split('.').length;
+
+    // 2 parts domains are names, the rest are subdomains
+    const parentNames = parentParts === 2 ? ownedNames : ownedSubdomains;
+
+    const parent = parentNames.find(({ name }) => name === parentName);
+    return parent?.id || null;
 }
