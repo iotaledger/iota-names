c7df0cda9073200d2673930748b3778abcfcc5b8
Keito <64607484+KeitoTadashi@users.noreply.github.com>
2025-07-03 16:13:17 +0200

    fix(dapp): Add grace period (#377)
    * feat(dapp): Add get user auctions metadata and render with Badges. Add claiming of won auctions. Add bidding on 'outbid' auctions. [WiP]
    
     Conflicts:
    	dapp/src/hooks/auction/useGetAddressAuctionHistory.ts
    	dapp/src/hooks/queryKey.ts
    
    * chore(dapp): Revert accidental changes
    
    * feat(dapp): Refactor auctions related codebase to 'auctions module' folder structure
    
    * feat(dapp): Refactor auction/lib/utils
    
    * chore(dapp): cleanup
    
    * chore(dapp): cleanup
    
    * chore(dapp): Impove useBidAuction query key
    
    * fix(dapp): Address review comments
    
    * fix(dapp): Remove unused state variable in UserAuctions
    
    * fix(dapp): Fix query key and add missing query invalidation
    
    * feat(dapp): Refactor auctionStatus logic in UserAuctions/AuctionItem
    
    * fix(dapp): Add loading indicator for claim transaction button
    
    * feat(dapp): add grace period for renewing tld
    
    * feat(dapp): grace period
    
    * chore: break line
    
    ---------
    
    Co-authored-by: Mario Sarcevic <mario.sarcevic@iota.org>
    Co-authored-by: marc2332 <mespinsanz@gmail.com>

--- dapp/src/components/dialogs/RenewNameDialog.tsx
+++ dapp/src/components/dialogs/RenewNameDialog.tsx
@@ -24,7 +24,7 @@ import {
     getNameObject,
     getNamePermissions,
     getParentObject,
-    isNameRecordExpired,
+    isGracePeriodExpired,
 } from '@/lib/utils/names';
 
 interface RenewDialogProps {
@@ -45,7 +45,7 @@ export function RenewNameDialog({ open, setOpen, name }: RenewDialogProps) {
         | undefined;
 
     const isNameSubName = nameRecord?.nameRecord ? isSubName(nameRecord.nameRecord.name) : null;
-    const isExpired = nameRecord?.nameRecord ? isNameRecordExpired(nameRecord?.nameRecord) : false;
+    const isExpired = nameRecord?.nameRecord ? isGracePeriodExpired(nameRecord?.nameRecord) : false;
     const namePermissions = nameRecord?.nameRecord
         ? getNamePermissions(nameRecord.nameRecord)
         : null;
