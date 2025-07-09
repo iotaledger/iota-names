fc47f00e9ad4ee6ef0edb725a0bffa21d007fd66
Juligs <julicodes29@gmail.com>
2025-07-09 12:33:23 +0200

    feat(dapp): add DropdownMenuOptions

---
+++ dapp/src/components/DropdownMenuOptions.tsx
@@ -0,0 +1,9 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+// import { Add, Assets, Calendar, Delete, Info, Link } from '@iota/apps-ui-icons';
+// import { Dropdown, ListItem } from '@iota/apps-ui-kit';
+
+// export function DropDownMenuOptions() {
+
+// }
