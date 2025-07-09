7940c1736b13f08ca7ae579e09b947e10bb60227
Juligs <julicodes29@gmail.com>
2025-07-09 14:37:46 +0200

    feat(dapp): add text styles

--- dapp/src/components/DropdownMenuOptions.tsx
+++ dapp/src/components/DropdownMenuOptions.tsx
@@ -8,7 +8,7 @@ interface DropdownMenuOptionProps {
 
 export function DropdownMenuOption({ icon, label }: DropdownMenuOptionProps) {
     return (
-        <div className="flex items-center gap-sm min-w-[262px]">
+        <div className="flex items-center gap-sm min-w-[262px] text-body-lg text-names-neutral-92">
             <span className="[&_svg]:h-6 [&_svg]:w-6">{icon}</span>
             <span>{label}</span>
         </div>
