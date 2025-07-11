7d607aa9ada9d04de8d5fd0e00523fafcbf43262
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-07-11 13:26:53 +0200

    feat: add right side panel UI

--- dapp/src/app/(protected)/my-names/[nft]/page.tsx
+++ dapp/src/app/(protected)/my-names/[nft]/page.tsx
@@ -3,17 +3,22 @@
 
 'use client';
 
-import { Close } from '@iota/apps-ui-icons';
+import { MoreHoriz } from '@iota/apps-ui-icons';
 import {
     Badge,
     BadgeType,
-    ButtonUnstyled,
+    ButtonType,
     Card,
+    CardAction,
+    CardActionType,
     CardBody,
+    CardImage,
     CardType,
+    Header,
+    ImageShape,
+    ImageType,
     LoadingIndicator,
     Panel,
-    Title,
 } from '@iota/apps-ui-kit';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useMemo, useState } from 'react';
@@ -117,22 +122,36 @@ export default function NftSubnamesPage({
 
                 {subnameListPanel && displayedSubname ? (
                     <div className="max-w-[360px] w-full">
-                        <Panel>
-                            <div className="w-full flex flex-row items-center justify-between pr-md--rs">
-                                <Title title={`Subnames for ${getNameLabel(displayedSubname)}`} />
-                                <ButtonUnstyled
-                                    className="text-names-neutral-70 hover:text-names-neutral-92"
-                                    onClick={() => {
-                                        setDisplayedSubname(undefined);
-                                    }}
-                                >
-                                    <Close />
-                                </ButtonUnstyled>
-                            </div>
-                            <div className="flex flex-col gap-lg px-md">
+                        <Panel bgColor="bg-names-neutral-6">
+                            <Header
+                                title={`Subnames for ${getNameLabel(displayedSubname)}`}
+                                onClose={() => {
+                                    setDisplayedSubname(undefined);
+                                }}
+                            />
+                            <div className="flex flex-col gap-xxs px-sm">
                                 {subnameListPanel.subnames.map((nft) => (
-                                    <Card key={nft.name} type={CardType.Filled}>
+                                    <Card
+                                        key={nft.name}
+                                        type={CardType.Default}
+                                        isHoverable
+                                        onClick={() => {}}
+                                    >
+                                        <CardImage
+                                            type={ImageType.BgTransparent}
+                                            shape={ImageShape.SquareRounded}
+                                        >
+                                            <div className="flex w-full h-full items-center justify-center bg-black rounded-lg">
+                                                <PlaceholderSvg />
+                                            </div>
+                                        </CardImage>
                                         <CardBody title={nft.name} />
+                                        <CardAction
+                                            type={CardActionType.Button}
+                                            buttonType={ButtonType.Ghost}
+                                            icon={<MoreHoriz />}
+                                            onClick={() => {}}
+                                        />
                                     </Card>
                                 ))}
                             </div>
@@ -142,4 +161,51 @@ export default function NftSubnamesPage({
             </div>
         </>
     ) : null;
+}
+
+function PlaceholderSvg() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+        >
+            <path
+                d="M14.8714 7.52631C15.0698 7.52631 15.2308 7.68743 15.2308 7.88617V12.3275C15.2308 12.4227 15.1929 12.5143 15.1254 12.5819L9.82064 17.8945C9.75316 17.962 9.66172 18 9.56631 18H5.13124C4.93278 18 4.77189 17.8388 4.77189 17.6401V13.1987C4.77189 13.1032 4.80979 13.0117 4.87727 12.9441L10.2874 7.52631H14.8714Z"
+                fill="url(#paint0_linear_11465_1911)"
+            />
+            <path
+                d="M9.928 2C10.1265 2 10.2874 2.16112 10.2874 2.35986V7.52598H5.12858C4.93012 7.52597 4.76923 7.36483 4.76923 7.16609V2.35986C4.76923 2.16113 4.93012 2.00001 5.12858 2H9.928Z"
+                fill="url(#paint1_linear_11465_1911)"
+            />
+            <defs>
+                <linearGradient
+                    id="paint0_linear_11465_1911"
+                    x1="13.6154"
+                    y1="6.69231"
+                    x2="2.1042"
+                    y2="13.4304"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stop-color="#D34BFF" />
+                    <stop offset="0.288525" stop-color="#278FFF" />
+                    <stop offset="0.673162" stop-color="#14F0D6" />
+                </linearGradient>
+                <linearGradient
+                    id="paint1_linear_11465_1911"
+                    x1="13.6154"
+                    y1="6.69231"
+                    x2="2.1042"
+                    y2="13.4304"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stop-color="#D34BFF" />
+                    <stop offset="0.288525" stop-color="#278FFF" />
+                    <stop offset="0.673162" stop-color="#14F0D6" />
+                </linearGradient>
+            </defs>
+        </svg>
+    );
 }
