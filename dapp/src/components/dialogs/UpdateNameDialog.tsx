ac4f41c42c88cc83c1886a6c8135b6dac01aa3d0
Keito <64607484+KeitoTadashi@users.noreply.github.com>
2025-07-15 13:08:48 +0200

    feat(dapp): merge useRegisterName hook into useUpdateName (#433)
    * feat(dapp): merge hooks and cleanup
    
    * fix error
    
    ---------
    
    Co-authored-by: msarcev <mario.sarcevic@iota.org>
    Co-authored-by: marc2332 <mespinsanz@gmail.com>

--- dapp/src/components/dialogs/UpdateNameDialog.tsx
+++ dapp/src/components/dialogs/UpdateNameDialog.tsx
@@ -172,7 +172,7 @@ export function UpdateNameDialog({ name, open, setOpen }: UpdateNameDialogProps)
         async mutationFn() {
             if (!updateNameTransaction) return;
             const transaction = await signAndExecuteTransaction({
-                transaction: updateNameTransaction,
+                transaction: updateNameTransaction.transaction,
             });
 
             await iotaClient.waitForTransaction({
