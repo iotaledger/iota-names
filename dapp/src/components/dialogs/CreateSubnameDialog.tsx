f96b86138d8894d9b59381211347de21cfc6ce95
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-07-16 13:07:41 +0200

    ci failing

--- dapp/src/components/dialogs/CreateSubnameDialog.tsx
+++ dapp/src/components/dialogs/CreateSubnameDialog.tsx
@@ -28,7 +28,7 @@ import { ChangeEvent, useState } from 'react';
 import { NameRecordData, queryKey, useNameRecord, useRegistrationNfts } from '@/hooks';
 import { NameUpdate, useUpdateNameTransaction } from '@/hooks/useUpdateNameTransaction';
 import { RegistrationNft } from '@/lib/interfaces';
-import { normalizeNameInput } from '@/lib/utils/format/formatNames';
+import { normalizeName } from '@/lib/utils/format/formatNames';
 import { getNameObject, isNameRecordExpired } from '@/lib/utils/names';
 
 function createSubnameUpdates({
@@ -49,7 +49,7 @@ function createSubnameUpdates({
     const isNameSubname = isSubname(nameRecord?.name || '');
 
     // Only join names if there user has written anything
-    const fullSubnameName = newSubname?.trim() ? normalizeNameInput(newSubname) + '.' + name : null;
+    const fullSubnameName = newSubname?.trim() ? normalizeName(newSubname) + '.' + name : null;
     // See if there is an existing subname with the same name
     const isSubnameAvailable = fullSubnameName
         ? getNameObject(ownedSubnames ?? [], fullSubnameName) === null
