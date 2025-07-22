8364b648c3d65bff6ca5b26c68a093cc95b3ad65
Juliana <115430927+Juligs@users.noreply.github.com>
2025-07-22 19:18:08 +0200

    feat(dapp): style landing page (#484)
    * feat(dapp): style landing page
    
    * feat(dapp): add more sections
    
    * feat(dapp): add image to built-for-builders
    
    * chore(dapp): enhance mobile layout
    
    * feat(dapp): resolve some comments and add circle-gradient
    
    * chore(dapp): rename circleDecoration to CircleGradient
    
    * chore(dapp): rename atributes SVG
    
    * chore(dapp): fix overflow and add padding in my names pages

---
+++ dapp/src/sections/NameUtility.tsx
@@ -0,0 +1,34 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { CircleGradient } from '@/components/CircleGradient';
+import { ImageCard, ImageCardSize } from '@/components/ImageCard';
+
+const WHY_IOTA_NAMES = [
+    {
+        title: 'Simplified, Safer Transactions',
+        body: 'Replace long, error-prone hashes with names people can recognize and remember',
+        image: '/landing-page/simplified-safer.png',
+    },
+    {
+        title: 'Unlimited Subnames, Infinite Utility',
+        body: 'Unlimited subnames let you set up custom tags like @wallet.yourname or @dev.yourname – ideal for services, roles, or communities',
+        image: '/landing-page/unlimited-subnames.png',
+    },
+];
+
+export function NameUtility() {
+    return (
+        <section className="relative overflow-hidden">
+            <div className="container py-14 md:py-20 items-center">
+                <div className="relative grid grid-cols-1 sm:grid-cols-2 gap-[33px]">
+                    <CircleGradient position="top-right" />
+                    <CircleGradient position="bottom-left" />
+                    {WHY_IOTA_NAMES.map((data, index) => (
+                        <ImageCard key={index} {...data} size={ImageCardSize.Large} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
