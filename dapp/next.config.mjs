03aa6e869f606b98a9da7586924cc96ee96236ea
evavirseda <evirseda@boxfish.studio>
2025-07-22 15:51:39 +0200

    embed git revision

--- dapp/next.config.mjs
+++ dapp/next.config.mjs
@@ -1,6 +1,15 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { execSync } from 'child_process';
+
+const NEXT_PUBLIC_IOTA_NAMES_REV = execSync('git rev-parse HEAD').toString().trim().toString();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
+    env: {
+        NEXT_PUBLIC_IOTA_NAMES_REV,
+    },
 };
 
 export default nextConfig;
