97ad91522cc4e0874164e526809a7dc3081cbaf7
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-08-07 11:43:55 +0200

    refactor(dapp): simplify `IotaNamesClientProvider` and `useIotaNamesClient` (#561)
    * refactor(dapp): simplify `IotaNamesClientProvider` context
    
    * fix: improve context type

--- dapp/src/contexts/IotaNamesClientContext.tsx
+++ dapp/src/contexts/IotaNamesClientContext.tsx
@@ -10,32 +10,6 @@ import { IotaGraphQLClient } from '@iota/iota-sdk/graphql';
 import React, { createContext, useContext, useMemo } from 'react';
 
 export const IotaNamesClientProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-    const { iotaNamesClient } = useIotaNamesClient();
-
-    return (
-        <IotaNamesClientContext.Provider value={{ iotaNamesClient }}>
-            {children}
-        </IotaNamesClientContext.Provider>
-    );
-};
-
-type IotaNamesClientContextType = {
-    iotaNamesClient: IotaNamesClient | null;
-};
-
-export const IotaNamesClientContext = createContext<IotaNamesClientContextType | null>(null);
-
-export function useIotaNamesClientContext(): IotaNamesClientContextType {
-    const context = useContext(IotaNamesClientContext);
-
-    if (!context) {
-        throw new Error('useIotaNamesClientContext must be used within a IotaNamesClientProvider');
-    }
-
-    return context;
-}
-
-export function useIotaNamesClient() {
     const ctx = useIotaClientContext();
     const network = getNetwork(ctx.network);
 
@@ -46,7 +20,27 @@ export function useIotaNamesClient() {
             }),
             network: network.id,
         });
-    }, [network]);
+    }, [network.graphql, network.id]);
 
-    return { iotaNamesClient };
+    return (
+        <IotaNamesClientContext.Provider value={{ iotaNamesClient }}>
+            {children}
+        </IotaNamesClientContext.Provider>
+    );
+};
+
+type IotaNamesClientContextType = {
+    iotaNamesClient: IotaNamesClient;
+};
+
+export const IotaNamesClientContext = createContext<IotaNamesClientContextType | null>(null);
+
+export function useIotaNamesClient(): IotaNamesClientContextType {
+    const context = useContext(IotaNamesClientContext);
+
+    if (!context) {
+        throw new Error('useIotaNamesClient must be used within a IotaNamesClientProvider');
+    }
+
+    return context;
 }
