91a534dc0acec40895c72ee950c5d706ace5c27c
marc2332 <mespinsanz@gmail.com>
2025-08-14 09:29:43 +0200

    chore(dapp): Update Terms and Conditions text and date

--- dapp/src/components/dialogs/TermsAndConditionsDialog.tsx
+++ dapp/src/components/dialogs/TermsAndConditionsDialog.tsx
@@ -36,24 +36,10 @@ export function TermsAndConditionsDialog({ open, onOpenChange }: TermsAndConditi
                     <div className="flex flex-col gap-xl">
                         <div className="flex flex-col gap-md">
                             <span className="text-label-md text-names-neutral-70">
-                                Effective Date: Lorem ipsum
+                                Effective Date: [RELEASE DATE]
                             </span>
                             <p className="text-body-md text-names-neutral-92">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac
-                                aliquam neque, quis ullamcorper ipsum. Name ac purus a magna
-                                ullamcorper venenatis ac eu nulla. Integer odio nunc, pretium sed
-                                nibh non, dignissim mattis metus. Donec quis bibendum mi. Donec sed
-                                tortor ullamcorper, tempus urna porta, sollicitudin nulla. Donec
-                                mattis lectus non consectetur sollicitudin. Nunc vel erat sit amet
-                                lectus condimentum fermentum. Vestibulum dapibus eros ut congue
-                                consectetur. Integer laoreet, leo id hendrerit accumsan, nulla
-                                mauris bibendum nisi, id hendrerit erat nunc porttitor erat.
-                                Pellentesque vitae purus ac urna lacinia commodo. In quis nulla
-                                dapibus, mattis metus ac, vulputate arcu. Vestibulum quis velit
-                                risus. Name lacus nisi, lobortis ac tristique eget, viverra ut
-                                libero. Nulla interdum, nibh at egestas fermentum, lorem lectus
-                                consectetur felis, nec rutrum nibh arcu vel lorem. Proin id urna sit
-                                amet odio luctus vulputate vestibulum ut diam.
+                                The IOTA Names Service does not collect, store, or process personal data. All interactions occur on a public blockchain. The Service does not use cookies, track users, or store IP addresses or off-chain identifiers.
                             </p>
                         </div>
                         <div className="flex flex-row items-center justify-between">
