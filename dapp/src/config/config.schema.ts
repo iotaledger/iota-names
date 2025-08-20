6c98445761aca5c981c885eb4c48c1433e131ec9
marc2332 <mespinsanz@gmail.com>
2025-08-20 09:43:04 +0200

    feat(dapp): Unify the config

---
+++ dapp/src/config/config.schema.ts
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { z } from 'zod';
+
+export const envSchema = z.record(
+    z.string(),
+    z.object({
+        network: z.enum(['devnet', 'testnet']),
+        indexerUrl: z.string(),
+        namesDisplayUrl: z.string(),
+    }),
+);
+
+export type Config = z.infer<typeof envSchema>;
