a17eb10c0d1acd104a883582b7d3ae354689b6f7
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-08-21 16:35:35 +0200

    refactor(dapp): use formatting utils from sdk

--- dapp/src/lib/utils/format/formatNanosToIota.ts
+++ dapp/src/lib/utils/format/formatNanosToIota.ts
@@ -1,12 +1,11 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { IOTA_DECIMALS } from '@iota/iota-sdk/utils';
-import BigNumber from 'bignumber.js';
+import { CoinFormat, formatBalance, IOTA_DECIMALS } from '@iota/iota-sdk/utils';
 
 import { IOTA_TOKEN_SYMBOL } from '@/lib/constants';
 
-interface FormatBalanceOptions {
+interface FormatNanosToIotaOptions {
     formatRounded?: boolean;
     showSign?: boolean;
     showIotaSymbol?: boolean;
@@ -14,104 +13,18 @@ interface FormatBalanceOptions {
 
 export function formatNanosToIota(
     balance: bigint | number | string,
-    { formatRounded = true, showSign = false, showIotaSymbol = true }: FormatBalanceOptions = {},
+    {
+        formatRounded = true,
+        showSign = false,
+        showIotaSymbol = true,
+    }: FormatNanosToIotaOptions = {},
 ) {
-    const bn = new BigNumber(balance.toString()).shiftedBy(-1 * IOTA_DECIMALS);
-    let formattedBalance = formatAmount(bn);
+    const formattedBalance = formatBalance(
+        balance,
+        IOTA_DECIMALS,
+        formatRounded ? CoinFormat.Rounded : CoinFormat.Full,
+        showSign,
+    );
 
-    if (!formatRounded) {
-        formattedBalance = bn.toFormat();
-    }
-
-    if (showSign && !formattedBalance.startsWith('-')) {
-        formattedBalance = `+${formattedBalance}`;
-    }
-
-    return `${formattedBalance}` + (showIotaSymbol ? ' ' + IOTA_TOKEN_SYMBOL : '');
+    return showIotaSymbol ? `${formattedBalance} ${IOTA_TOKEN_SYMBOL}` : formattedBalance;
 }
-
-export function formatAmount(...args: Parameters<typeof formatAmountParts>) {
-    return formatAmountParts(...args)
-        .filter(Boolean)
-        .join(' ');
-}
-
-export function formatAmountParts(amount?: BigNumber | bigint | number | string | null): string[] {
-    if (typeof amount === 'undefined' || amount === null) {
-        return ['--'];
-    }
-
-    let postfix = '';
-    let bn = new BigNumber(amount.toString());
-    const bnAbs = bn.abs();
-
-    // use absolute value to determine the postfix
-    if (bnAbs.gte(1_000_000_000)) {
-        bn = bn.shiftedBy(-9);
-        postfix = 'B';
-    } else if (bnAbs.gte(1_000_000)) {
-        bn = bn.shiftedBy(-6);
-        postfix = 'M';
-    } else if (bnAbs.gte(10_000)) {
-        bn = bn.shiftedBy(-3);
-        postfix = 'K';
-    }
-
-    if (bnAbs.gte(1)) {
-        bn = bn.decimalPlaces(2, BigNumber.ROUND_DOWN);
-    }
-
-    if (bnAbs.gt(0) && bnAbs.lt(1)) {
-        const leadingZeros = countDecimalLeadingZeros(bn.toFormat());
-
-        if (leadingZeros >= 4) {
-            return [formatWithSubscript(bn.toFormat(), leadingZeros), postfix];
-        } else {
-            return [bn.toFormat(leadingZeros + 1), postfix];
-        }
-    }
-
-    return [bn.toFormat(), postfix];
-}
-
-export const countDecimalLeadingZeros = (
-    input: BigNumber | bigint | number | string | null,
-): number => {
-    if (input === null) {
-        return 0;
-    }
-
-    const [, decimals] = input.toString().split('.');
-
-    if (!decimals) {
-        return 0;
-    }
-
-    let count = 0;
-
-    for (const digit of decimals) {
-        if (digit === '0') {
-            count++;
-        } else {
-            break;
-        }
-    }
-
-    return count;
-};
-
-const SUBSCRIPTS = ['₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉'];
-
-export const formatWithSubscript = (
-    input: BigNumber | bigint | number | string | null,
-    zeroCount: number,
-): string => {
-    if (input === null) {
-        return '0';
-    }
-
-    const [, decimals] = input.toString().split('.');
-    const remainder = decimals.slice(zeroCount);
-
-    return `0.0${SUBSCRIPTS[zeroCount]}${remainder}`;
-};
