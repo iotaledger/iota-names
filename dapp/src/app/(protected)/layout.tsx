2ba7af14fc26a7d13a0d06fa105eaa2100a4d3d7
marc2332 <mespinsanz@gmail.com>
2025-08-26 11:17:01 +0200

    :godmode:

--- dapp/src/app/(protected)/layout.tsx
+++ dapp/src/app/(protected)/layout.tsx
@@ -3,18 +3,31 @@
 
 'use client';
 
-import { useCurrentAccount } from '@iota/dapp-kit';
+import { LoadingIndicator } from '@iota/apps-ui-kit';
+import { useAutoConnectWallet, useCurrentWallet } from '@iota/dapp-kit';
 import { redirect } from 'next/navigation';
-import { useEffect, type PropsWithChildren } from 'react';
+import { useEffect, useState, type PropsWithChildren } from 'react';
 
 import { CONNECT_ROUTE } from '@/lib/constants';
 import { TermsAndConds } from '@/lib/utils/termsAndConditions';
 
 export default function ProtectedLayout({ children }: PropsWithChildren) {
-    const currentAccount = useCurrentAccount();
+    const { isConnecting, isConnected, isDisconnected } = useCurrentWallet();
+    const autoConnect = useAutoConnectWallet();
+    const [status, setStatus] = useState<'idle' | 'connecting' | 'disconnected'>('idle');
+
+    if (status === 'idle') {
+        if (autoConnect === 'attempted-connection' && isConnecting) {
+            // Auto connect found a wallet and it started connecting
+            setStatus('connecting');
+        } else if (autoConnect === 'attempted') {
+            // Auto could not make a successful connection
+            setStatus('disconnected');
+        }
+    }
 
     useEffect(() => {
-        if (!currentAccount) {
+        if (status !== 'idle' && isDisconnected) {
             return redirect(CONNECT_ROUTE.path);
         }
 
@@ -22,13 +35,23 @@ export default function ProtectedLayout({ children }: PropsWithChildren) {
         if (!areAccepted) {
             redirect('/?modal=terms_conditions');
         }
-    }, [currentAccount]);
+    }, [status, isDisconnected]);
 
-    return (
+    if (autoConnect === 'idle') {
+        return (
+            <div className="flex h-screen w-full justify-center">
+                <LoadingIndicator size="w-16 h-16" />
+            </div>
+        );
+    }
+
+    console.log(autoConnect);
+
+    return isConnected ? (
         <main className="flex flex-col min-h-screen">
             <div className="container w-full h-full pt-20 flex flex-col flex-1">
                 <div className="flex flex-col w-full gap-y-lg py-lg flex-1">{children}</div>
             </div>
         </main>
-    );
+    ) : null;
 }
