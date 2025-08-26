74540799bacee93077103735d4d6a144a7ecfd93
Marc Espin <mespinsanz@gmail.com>
2025-08-26 16:00:02 +0200

    feat(dapp): Unify the config (#649)
    * feat(dapp): Unify the config
    
    * chore: Update names of jobs
    
    * chore: Update mocked envs in typescript ci
    
    * dprint fmt
    
    * staging is back
    
    * move vercel alias
    
    * move secrets passing
    
    * support manual workflow call of staging
    
    * temp trigger
    
    * swap input and  secret in vercel deploy
    
    * ALIAS_DOMAINS
    
    * add mainnet to config schema
    
    * NEXT_PUBLIC_NAMES_DAPP_DEFAULT_NETWORK
    
    * debug config
    
    * use prebuilt
    
    * build with vercel cli
    
    * pull project settings
    
    * log config
    
    * ftm
    
    * "
    
    * get vercel alias from secrets
    
    * switch staging to devnet
    
    * attempt to heal the pnpm lonk file
    
    * clean up
    
    * actually reset pnpm lock file
    
    * add base url to config
    
    * add base url to mocked env in ts build step
    
    * Update .env.example
    
    * remove this branch from triggering staging deploys
    
    * Update dapp/src/config/config.ts
    
    Co-authored-by: Panteley <panteleymonchuk@gmail.com>
    
    * fmt
    
    ---------
    
    Co-authored-by: Panteley <panteleymonchuk@gmail.com>

--- dapp/src/lib/utils/displayImage.ts
+++ dapp/src/lib/utils/displayImage.ts
@@ -1,7 +1,9 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+import { CONFIG } from '@/config';
+
 export function getNameDisplaySrc(name: string, expirationTimestamp: number) {
-    const baseUrl = process.env.NEXT_PUBLIC_NAMES_DISPLAY_API_URL;
+    const baseUrl = CONFIG.namesDisplayUrl;
     return new URL(`${name}/${expirationTimestamp}`, baseUrl).href;
 }
