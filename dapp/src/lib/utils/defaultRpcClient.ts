23cf999a3ce6a2b051b1a9061a793e67757258d6
Branko <brankobosnic1@gmail.com>
2025-09-02 15:22:11 +0200

    fix: add dryRunTransactionBlock to unsupported methods

--- dapp/src/lib/utils/defaultRpcClient.ts
+++ dapp/src/lib/utils/defaultRpcClient.ts
@@ -17,7 +17,7 @@ export const createIotaClient = (network: NetworkId): IotaClient => {
     // If network is not supported, we use assume we are using a custom RPC
     const networkGraphqlUrl = supportedNetwork?.graphql ?? network;
     const networkJsonRpcUrl = supportedNetwork?.url;
-
+    // console.log(RPC_METHODS);
     const client = new IotaClient({
         transport: new IotaClientGraphQLTransport({
             url: networkGraphqlUrl,
@@ -27,6 +27,7 @@ export const createIotaClient = (network: NetworkId): IotaClient => {
                 'getReferenceGasPrice',
                 'getNormalizedMoveFunction',
                 'getOwnedObjects',
+                'dryRunTransactionBlock',
             ],
         }),
     });
