379b32f24f75bdb7070d94155cf5e32ce369d29a
marc2332 <mespinsanz@gmail.com>
2025-09-03 18:13:49 +0200

    chore(dapp): Add Documentation to navbar

--- dapp/src/lib/enums/routeTitle.enums.ts
+++ dapp/src/lib/enums/routeTitle.enums.ts
@@ -1,7 +1,8 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-export enum ProtectedRouteTitle {
+export enum RouteTitle {
     MyNames = 'My Names',
     Auction = 'Auctions',
+    Documentation = 'Documentation',
 }
