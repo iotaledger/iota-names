a3aa47adc1270460ff472d368c6113b6b63dcfe4
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-09-03 11:41:44 +0200

    fix: remove comma separator

--- dapp/src/lib/utils/format/formatNanosToIota.ts
+++ dapp/src/lib/utils/format/formatNanosToIota.ts
@@ -2,13 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { CoinFormat, formatBalance, IOTA_DECIMALS } from '@iota/iota-sdk/utils';
+import { BigNumber } from 'bignumber.js';
 
 import { IOTA_TOKEN_SYMBOL } from '@/lib/constants';
+
+const defaultConfig = BigNumber.config();
+
+function resetBigNumberConfig() {
+    BigNumber.config(defaultConfig);
+}
 
 interface FormatNanosToIotaOptions {
     formatRounded?: boolean;
     showSign?: boolean;
     showIotaSymbol?: boolean;
+    hasCommaSeparator?: boolean;
 }
 
 export function formatNanosToIota(
@@ -17,8 +25,17 @@ export function formatNanosToIota(
         formatRounded = true,
         showSign = false,
         showIotaSymbol = true,
+        hasCommaSeparator = true,
     }: FormatNanosToIotaOptions = {},
 ) {
+    if (!hasCommaSeparator) {
+        BigNumber.config({
+            FORMAT: {
+                groupSeparator: '',
+            },
+        });
+    }
+
     const formattedBalance = formatBalance(
         balance,
         IOTA_DECIMALS,
@@ -26,5 +43,6 @@ export function formatNanosToIota(
         showSign,
     );
 
+    resetBigNumberConfig();
     return showIotaSymbol ? `${formattedBalance} ${IOTA_TOKEN_SYMBOL}` : formattedBalance;
 }
