d51cbb14aeb9eb6589461edb6813dea7e06bc28b
Juligs <julicodes29@gmail.com>
2025-09-04 09:15:21 +0200

    chore(dapp): add important

--- dapp/src/components/skeletons/CardSkeletonLoader.tsx
+++ dapp/src/components/skeletons/CardSkeletonLoader.tsx
@@ -21,24 +21,24 @@ export function CardSkeletonLoader({ isAuctionCard }: skeletonCardProps) {
                 >
                     <Skeleton className="w-full h-[220px] !rounded-xl" />
                     {isAuctionCard ? (
-                        <div className="flex flex-col p-md gap-xs name-card-shadow w-full flex-1">
-                            <Skeleton className="w-2/4 h-4 !rounded-lg" />
+                        <div className="flex flex-col p-md gap-xs name-card-shadow flex-1">
+                            <Skeleton className="!w-2/4 h-4 !rounded-lg" />
                             <div className="flex justify-between items-center">
-                                <Skeleton className="w-1/4 h-4 !rounded-lg" />
-                                <Skeleton className="w-14 h-9 !rounded-full" />
+                                <Skeleton className="!w-1/4 h-4 !rounded-lg" />
+                                <Skeleton className="!w-14 h-9 !rounded-full" />
                             </div>
-                            <Skeleton className="w-2/4 h-4 !rounded-lg" />
-                            <Skeleton className="w-full h-1 !rounded-lg my-4" />
+                            <Skeleton className="!w-2/4 h-4 !rounded-lg" />
+                            <Skeleton className="!w-full h-[2px] !rounded-lg my-4" />
                             <div className="flex justify-between items-center">
-                                <Skeleton className="w-1/4 h-4 !rounded-lg" />
-                                <Skeleton className="w-14 h-4 !rounded-lg" />
+                                <Skeleton className="!w-1/4 h-4 !rounded-lg" />
+                                <Skeleton className="!w-14 h-4 !rounded-lg" />
                             </div>
                         </div>
                     ) : (
                         <div className="flex flex-col p-md gap-md name-card-shadow w-full flex-1">
-                            <Skeleton className="w-2/4 h-4 !rounded-lg" />
-                            <Skeleton className="w-1/4 h-3 !rounded-lg" />
-                            <Skeleton className="w-full h-9 !rounded-full" />
+                            <Skeleton className="!w-2/4 h-4 !rounded-lg" />
+                            <Skeleton className="!w-1/4 h-3 !rounded-lg" />
+                            <Skeleton className="!w-full h-9 !rounded-full" />
                         </div>
                     )}
                 </div>
