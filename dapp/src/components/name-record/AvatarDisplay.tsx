6813148bdd3061352e1763851b195a35ba8d093c
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-09-24 13:36:59 +0200

    fix(dapp): fix infinite render loop

--- dapp/src/components/name-record/AvatarDisplay.tsx
+++ dapp/src/components/name-record/AvatarDisplay.tsx
@@ -1,12 +1,11 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { isSubname } from '@iota/iota-names-sdk';
 import cx from 'clsx';
 import { useEffect, useState } from 'react';
 
 import loadingAnimationData from '@/animations/lottie-loading.json';
-import { useNameRecord, useRegistrationNfts } from '@/hooks';
+import { useNameRecord } from '@/hooks';
 import { useGetObject } from '@/hooks/useGetOwnedObject';
 
 import { LottieAnimation } from '../loaders/Lottie';
@@ -19,16 +18,12 @@ interface NameAvatarDisplay {
 
 export function NameAvatarDisplay({ name }: NameAvatarDisplay) {
     const { data: nameRecordData, isLoading: isNameRecordDataLoading } = useNameRecord(name);
-    const { data: subnames, isLoading: isSubnamesLoading } = useRegistrationNfts('subname');
-    const isNameSubname = isSubname(name);
 
     const avatarId =
         nameRecordData?.type === 'unavailable'
             ? nameRecordData?.nameRecord.avatar
                 ? nameRecordData.nameRecord.avatar
-                : isNameSubname
-                  ? subnames?.find((n) => n.name === name)?.id
-                  : nameRecordData.nameRecord.nftId
+                : nameRecordData.nameRecord.nftId
             : null;
 
     const { data: avatarObject, isLoading: isAvatarLoading } = useGetObject(name, {
@@ -36,7 +31,7 @@ export function NameAvatarDisplay({ name }: NameAvatarDisplay) {
         options: { showDisplay: true, showContent: true },
     });
 
-    const isDataLoading = isNameRecordDataLoading || isSubnamesLoading || isAvatarLoading;
+    const isDataLoading = isNameRecordDataLoading || isAvatarLoading;
 
     return (
         <AvatarDisplay
