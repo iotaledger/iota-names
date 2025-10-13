1383757297a353d0f1a067516ceec58d99ff66c8
Mario Sarcevic <mario.sarcevic@iota.org>
2025-10-13 12:53:02 +0200

    feat(dapp): Bootstrap playwright e2e tests

---
+++ dapp/tests/example.spec.ts
@@ -0,0 +1,22 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { expect, test } from './helpers/fixtures';
+
+test.describe('Basic e2e tests', () => {
+    test('should load the application', async ({ appPage }) => {
+        await appPage.waitForLoadState('networkidle');
+        await expect(appPage).toHaveTitle(/IOTA Names/i);
+    });
+
+    test('should have IOTA Wallet extension loaded', async ({ extensionId, extensionContext }) => {
+        expect(extensionId).toBeTruthy();
+        expect(extensionId).toMatch(/^[a-z]{32}$/);
+
+        const extensionUrl = `chrome-extension://${extensionId}/ui.html`;
+        const extensionPage = await extensionContext.newPage();
+        await extensionPage.goto(extensionUrl);
+
+        await expect(extensionPage).toHaveURL(extensionUrl);
+    });
+});
