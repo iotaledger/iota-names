4427808868b6b6bf0f2d2bc2dfe808f458e6abed
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-11-06 17:03:46 +0100

    sync dapp

--- dapp/src/auctions/hooks/useAuctions.ts
+++ dapp/src/auctions/hooks/useAuctions.ts
@@ -104,7 +104,7 @@ export function useAuctions({
     });
 
     const { auctionsTableObjectId } = auctionHouseData || {};
-    const { packageId } = iotaNamesClient.config;
+    const packageId = iotaNamesClient.resolvePackageId('v1');
 
     // Then, fetch metadata for each auction
     const combinedResult = useQueries({
