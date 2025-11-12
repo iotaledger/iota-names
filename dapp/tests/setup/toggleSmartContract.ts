8fe7d39f40c9fbcb322be4d388730f9b348b6d9a
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-11-12 12:49:20 +0100

    feat(dapp): add scripts to manage smart contract flags for tests (#896)
    * feat(dapp): add connection test and helpers
    
    * feat: make tests not fully parallel
    
    * feat: add scripts to manage smart contract flags for tests

---
+++ dapp/tests/setup/toggleSmartContract.ts
@@ -0,0 +1,86 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { expect } from '@playwright/test';
+
+import { getAuthorizedSmartContractTypes, iotaNamesClient, runCommand } from './utils';
+
+const envs = {
+    IOTA_NAMES_PACKAGE_ADDRESS: iotaNamesClient.config.packageId,
+    IOTA_NAMES_OBJECT_ID: iotaNamesClient.config.iotaNamesObjectId,
+    ADMIN_CAP: iotaNamesClient.config.adminCap,
+    IOTA_NAMES_PAYMENTS_PACKAGE_ADDRESS: iotaNamesClient.config.paymentsPackageId,
+    ADMIN_ADDRESS: iotaNamesClient.config.adminAddress,
+    IOTA_NAMES_AUCTIONS_PACKAGE_ADDRESS: iotaNamesClient.config.auctionPackageId,
+};
+
+function getUpdateAuthCommand(mode: 'enable' | 'disable', type: 'payment' | 'auction') {
+    const authorize = mode === 'enable';
+
+    return [
+        `iota client ptb --move-call $IOTA_NAMES_PACKAGE_ADDRESS::iota_names::${authorize ? 'authorize' : 'deauthorize'} ` +
+            (type === 'payment'
+                ? '"<$IOTA_NAMES_PAYMENTS_PACKAGE_ADDRESS::payments::PaymentsAuth>"'
+                : '"<$IOTA_NAMES_AUCTIONS_PACKAGE_ADDRESS::auction::AuctionAuth>"') +
+            ' @$ADMIN_CAP @$IOTA_NAMES_OBJECT_ID --sender @$ADMIN_ADDRESS',
+    ].join(' \\\n  ');
+}
+
+export async function toggleSmartContractMode(mode: 'auctions' | 'purchases'): Promise<void> {
+    const targetState = {
+        isAuctionAuthorized: mode === 'auctions',
+        isPaymentAuthorized: mode === 'purchases',
+    };
+
+    const allCommands = {
+        auction: {
+            enable: getUpdateAuthCommand('enable', 'auction'),
+            disable: getUpdateAuthCommand('disable', 'auction'),
+        },
+        payment: {
+            enable: getUpdateAuthCommand('enable', 'payment'),
+            disable: getUpdateAuthCommand('disable', 'payment'),
+        },
+    };
+
+    const currentState = await getAuthorizedSmartContractTypes();
+
+    if (currentState.isAuctionAuthorized !== targetState.isAuctionAuthorized) {
+        const action = targetState.isAuctionAuthorized ? 'enable' : 'disable';
+        const command = allCommands.auction[action];
+        await execTryCatch(command, envs);
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+
+    if (currentState.isPaymentAuthorized !== targetState.isPaymentAuthorized) {
+        const action = targetState.isPaymentAuthorized ? 'enable' : 'disable';
+        const command = allCommands.payment[action];
+        await execTryCatch(command, envs);
+    }
+
+    if (
+        currentState.isAuctionAuthorized === targetState.isAuctionAuthorized &&
+        currentState.isPaymentAuthorized === targetState.isPaymentAuthorized
+    ) {
+        console.log('Smart contract modes are already in the target state. No changes made.');
+        return;
+    }
+
+    const finalState = await getAuthorizedSmartContractTypes();
+
+    expect(finalState.isAuctionAuthorized).toBe(targetState.isAuctionAuthorized);
+    expect(finalState.isPaymentAuthorized).toBe(targetState.isPaymentAuthorized);
+}
+
+async function execTryCatch(...args: Parameters<typeof runCommand>): Promise<void> {
+    try {
+        const output = await runCommand(...args);
+        console.log('Command output:', output);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        }
+        expect(error).toBeFalsy();
+    }
+}
