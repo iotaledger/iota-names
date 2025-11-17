ef73231ce04cad71b22850fc011007fb5314dc24
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-11-17 19:37:23 +0100

    feat(dapp): create subname test

--- dapp/tests/management/management.spec.ts
+++ dapp/tests/management/management.spec.ts
@@ -1,10 +1,11 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+import { Ed25519Keypair } from '@iota/iota-sdk/keypairs/ed25519';
 import { formatAddress } from '@iota/iota-sdk/utils';
 
 import { expect, test } from '../helpers/fixtures';
-import { connectWallet, createWallet, requestFaucetTokens } from '../utils';
+import { connectWallet, createWallet, purchaseName, requestFaucetTokens } from '../utils';
 
 test.describe.parallel('Name Management Tests', () => {
     test.beforeAll(async ({ appPage, context, extensionPage, extensionName, sharedState }) => {
@@ -22,5 +23,40 @@ test.describe.parallel('Name Management Tests', () => {
 
         sharedState.wallet.address = address;
         sharedState.wallet.mnemonic = mnemonic;
+    });
+
+    test('Create subname', async ({ appPage: page, context, sharedState }) => {
+        const keypair = Ed25519Keypair.deriveKeypair(sharedState.wallet.mnemonic ?? '');
+        const name = `e2etest-${Math.floor(Math.random() * 100)}.iota`;
+
+        const { response } = await purchaseName(name, sharedState.wallet.address ?? '', keypair);
+        expect(response.effects?.status.status).toBe('success');
+
+        await page.goto('/my-names');
+        const nameCards = page.getByTestId('name-card');
+        await expect(nameCards.first()).toBeVisible({ timeout: 10_000 });
+        const nameCard = page.getByTestId('name-card').first();
+        await nameCard.getByTestId('name-card-avatar').hover();
+        const menuButtonLocator = nameCard.getByTestId('menu-button');
+        await expect(menuButtonLocator).toBeVisible();
+        await menuButtonLocator.click();
+
+        await page.getByText('Create Subname', { exact: true }).click();
+
+        const dialog = page.getByRole('dialog');
+        await expect(dialog.getByText('New Subname')).toBeVisible();
+
+        const subname = 'sub-' + Math.floor(Math.random() * 1000);
+        await dialog.getByPlaceholder('Enter subname').fill(subname);
+
+        await dialog.getByRole('button', { name: 'Create' }).click();
+        (await context.waitForEvent('page')).getByRole('button', { name: 'Approve' }).click();
+        await page.bringToFront();
+
+        await expect(page.getByText('Successfully created subname', { exact: false })).toBeVisible({
+            timeout: 30_000,
+        });
+
+        await page.close();
     });
 });
