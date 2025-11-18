6215a31b93742c4623ae087138bf7eb173484cc1
Juligs <julicodes29@gmail.com>
2025-11-18 10:00:50 +0100

    chore(dapp): remove duplicate id

--- dapp/src/app/auctions/page.tsx
+++ dapp/src/app/auctions/page.tsx
@@ -225,10 +225,7 @@ export default function AuctionsPage(): JSX.Element {
                 <h2 className="text-headline-md text-names-neutral-92 font-bold leading-[120%] -tracking-[0.4px]">
                     Auctions
                 </h2>
-                <div
-                    className="w-[40px] h-[40px] flex items-center justify-center"
-                    data-testid="auctions-refresh-container"
-                >
+                <div className="w-[40px] h-[40px] flex items-center justify-center">
                     {selectedStatus === 'active' || selectedStatus === 'all' ? (
                         <Button
                             type={ButtonType.Outlined}
