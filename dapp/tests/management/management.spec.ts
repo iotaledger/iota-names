06dbb5b9e58a547c3b14bc146e0abeb4707003da
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-11-19 00:24:02 +0100

    feat(dapp): set permissions tests

--- dapp/tests/management/management.spec.ts
+++ dapp/tests/management/management.spec.ts
@@ -1,10 +1,21 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+import { normalizeIotaName } from '@iota/iota-names-sdk';
+import { Ed25519Keypair } from '@iota/iota-sdk/keypairs/ed25519';
 import { formatAddress } from '@iota/iota-sdk/utils';
 
 import { expect, test } from '../helpers/fixtures';
-import { connectWallet, createWallet, requestFaucetTokens } from '../utils';
+import { iotaNamesClient } from '../setup/utils';
+import {
+    addSubnameName,
+    connectWallet,
+    createWallet,
+    generateRandomName,
+    generateRandomSubname,
+    purchaseName,
+    requestFaucetTokens,
+} from '../utils';
 
 test.describe.parallel('Name Management Tests', () => {
     test.beforeAll(async ({ appPage, context, extensionPage, extensionName, sharedState }) => {
@@ -22,5 +33,68 @@ test.describe.parallel('Name Management Tests', () => {
 
         sharedState.wallet.address = address;
         sharedState.wallet.mnemonic = mnemonic;
+    });
+
+    test('Set permissions to a subname', async ({ appPage: page, context, sharedState }) => {
+        const keypair = Ed25519Keypair.deriveKeypair(sharedState.wallet.mnemonic ?? '');
+        const name = generateRandomName('display');
+        const subname = generateRandomSubname('subname', name);
+        const responsePurchase = await purchaseName(
+            name,
+            sharedState.wallet.address ?? '',
+            keypair,
+        );
+        expect(responsePurchase.effects?.status.status).toBe('success');
+
+        const record = await iotaNamesClient.getNameRecord(name);
+        if (!record) throw new Error('Name record not found');
+
+        const responsePurchaseSubname = await addSubnameName(
+            subname,
+            record.nftId,
+            record.expirationDate,
+            sharedState.wallet.address ?? '',
+            keypair,
+        );
+        expect(responsePurchaseSubname.effects?.status.status).toBe('success');
+
+        await page.goto('/my-names');
+        await expect(
+            page.getByTestId('name-card').filter({ hasText: normalizeIotaName(subname, 'at') }),
+        ).toBeVisible({ timeout: 10_000 });
+
+        const nameCard = page
+            .getByTestId('name-card')
+            .filter({ hasText: normalizeIotaName(subname, 'at') });
+
+        await nameCard.getByTestId('name-card-avatar').hover();
+        const menuButtonLocator = nameCard.getByTestId('menu-button');
+        await expect(menuButtonLocator).toBeVisible();
+        await menuButtonLocator.click();
+
+        await page.getByText('Set Permissions', { exact: true }).click();
+        const dialog = page.getByRole('dialog');
+        await expect(dialog.getByText('Set permissions')).toBeVisible();
+
+        const allowSubnamesLabel = dialog.getByText('Allow Subname to create additional Subnames');
+        await expect(allowSubnamesLabel).toBeVisible();
+        await allowSubnamesLabel.click();
+
+        const allowRenewLabel = dialog.getByText('Allow Subname to renew expiration');
+        await expect(allowRenewLabel).toBeVisible();
+        await allowRenewLabel.click();
+
+        const saveBtn = dialog.getByRole('button', { name: 'Save' });
+        await expect(saveBtn).toBeEnabled();
+
+        await dialog.getByRole('button', { name: 'Save' }).click();
+        (await context.waitForEvent('page')).getByRole('button', { name: 'Approve' }).click();
+        await page.bringToFront();
+
+        await expect(
+            page.getByText('Permissions updated successfully', { exact: false }),
+        ).toBeVisible({ timeout: 30_000 });
+
+        await page.close();
     });
 });
