1c5412e011a4a22353798fbbab6e250834dfbe13
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-11-18 13:31:50 +0100

    feat(dapp): polish

--- dapp/tests/management/management.spec.ts
+++ dapp/tests/management/management.spec.ts
@@ -1,11 +1,18 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+import { normalizeIotaName } from '@iota/iota-names-sdk';
 import { Ed25519Keypair } from '@iota/iota-sdk/keypairs/ed25519';
 import { formatAddress } from '@iota/iota-sdk/utils';
 
 import { expect, test } from '../helpers/fixtures';
-import { connectWallet, createWallet, purchaseName, requestFaucetTokens } from '../utils';
+import {
+    connectWallet,
+    createWallet,
+    generateRandomName,
+    purchaseName,
+    requestFaucetTokens,
+} from '../utils';
 
 test.describe.parallel('Name Management Tests', () => {
     test.beforeAll(async ({ appPage, context, extensionPage, extensionName, sharedState }) => {
@@ -27,7 +34,7 @@ test.describe.parallel('Name Management Tests', () => {
 
     test('View name info', async ({ appPage: page, sharedState }) => {
         const keypair = Ed25519Keypair.deriveKeypair(sharedState.wallet.mnemonic ?? '');
-        const name = `e2etest-${Math.floor(Math.random() * 100)}.iota`;
+        const name = generateRandomName('display');
 
         const { response } = await purchaseName(name, sharedState.wallet.address ?? '', keypair);
         expect(response.effects?.status.status).toBe('success');
@@ -36,7 +43,9 @@ test.describe.parallel('Name Management Tests', () => {
         const nameCards = page.getByTestId('name-card');
         await expect(nameCards.first()).toBeVisible({ timeout: 10_000 });
 
-        const nameCard = page.getByTestId('name-card').first();
+        const nameCard = page
+            .getByTestId('name-card')
+            .filter({ hasText: normalizeIotaName(name, 'at') });
         await nameCard.getByTestId('name-card-avatar').hover();
         const menuButtonLocator = nameCard.getByTestId('menu-button');
         await expect(menuButtonLocator).toBeVisible();
