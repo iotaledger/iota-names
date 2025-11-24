af8df5d7e3a96d911df07d558003d6667dcb32c3
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-11-24 09:40:22 +0100

    fix: get correct contracts

--- dapp/tests/common/validation.utils.ts
+++ dapp/tests/common/validation.utils.ts
@@ -5,28 +5,36 @@ import { execTryCatch } from '../setup/toggleSmartContract';
 import { iotaNamesClient } from '../setup/utils';
 
 export async function addBlockedLabels(labels: string[]) {
+    const packageId = iotaNamesClient.getPackage('packageId');
+    const iotaNamesObjectId = iotaNamesClient.getPackage('iotaNamesObjectId');
+    const adminCap = iotaNamesClient.getPackage('adminCap');
+
     const formattedLabels = `['${labels.join("', '")}']`;
 
     const command = `iota client ptb \
         --make-move-vec "<0x1::string::String>" "${formattedLabels}" \
         --assign labels \
-        --move-call ${iotaNamesClient.config.packageId}::deny_list::add_blocked_labels \
-            @${iotaNamesClient.config.iotaNamesObjectId} \
-            @${iotaNamesClient.config.adminCap} \
+        --move-call ${packageId}::deny_list::add_blocked_labels \
+            @${iotaNamesObjectId} \
+            @${adminCap} \
             labels`;
 
     return await execTryCatch(command.replace(/\n/g, ' '));
 }
 
 export async function addReservedLabels(labels: string[]) {
+    const packageId = iotaNamesClient.getPackage('packageId');
+    const iotaNamesObjectId = iotaNamesClient.getPackage('iotaNamesObjectId');
+    const adminCap = iotaNamesClient.getPackage('adminCap');
+
     const formattedLabels = `['${labels.join("', '")}']`;
 
     const command = `iota client ptb \
         --make-move-vec "<0x1::string::String>" "${formattedLabels}" \
         --assign labels \
-        --move-call ${iotaNamesClient.config.packageId}::deny_list::add_reserved_labels \
-            @${iotaNamesClient.config.iotaNamesObjectId} \
-            @${iotaNamesClient.config.adminCap} \
+        --move-call ${packageId}::deny_list::add_reserved_labels \
+            @${iotaNamesObjectId} \
+            @${adminCap} \
             labels`;
 
     return await execTryCatch(command.replace(/\n/g, ' '));
