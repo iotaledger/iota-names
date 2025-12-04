7d4766262ea38cb4366dae736c470fbe016381a9
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-12-04 13:47:56 +0100

    fix: wallet text

--- dapp/tests/utils.ts
+++ dapp/tests/utils.ts
@@ -40,7 +40,7 @@ export async function createWallet(page: Page) {
     await page.bringToFront();
     await page.getByRole('button', { name: /Get Started/ }).click({ timeout: 30_000 });
     await page.getByText('Create a new wallet').click();
-    await page.getByText('Recovery Phrase', { exact: true }).click();
+    await page.getByText('Mnemonic', { exact: true }).click();
     await page.getByTestId('password.input').fill('iotae2etests');
     await page.getByTestId('password.confirmation').fill('iotae2etests');
     await page.getByText('I read and agree').click();
