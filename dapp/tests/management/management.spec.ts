03b4e5f948cfca058b3c5d4822fd69004103110e
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-12-17 15:10:19 +0100

    deparallelize tests

--- dapp/tests/management/management.spec.ts
+++ dapp/tests/management/management.spec.ts
@@ -28,7 +28,7 @@ import {
 } from '../utils';
 
 test.setTimeout(60_000);
-test.describe.parallel('Name Management Tests', () => {
+test.describe.serial('Name Management Tests', () => {
     test.beforeAll(async ({ appPage, context, extensionPage, extensionName, sharedState }) => {
         const { address, mnemonic } = await createWallet(extensionPage);
 
