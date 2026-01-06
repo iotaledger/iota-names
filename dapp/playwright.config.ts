bdd587fc333042a6af4acf4c581d21492692c52e
Mario Sarcevic <mario.sarcevic@iota.org>
2026-01-06 11:08:30 +0100

    fix(ci): What if we increased the retries to 2 ?

--- dapp/playwright.config.ts
+++ dapp/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     testDir: './tests',
     fullyParallel: false,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 1 : 0,
+    retries: process.env.CI ? 2 : 0,
     workers: 1,
     reporter: 'html',
     expect: {
