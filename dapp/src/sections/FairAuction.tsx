8d582e5289cadb09626cfa3ff2f4f57a3d8e547b
JCNoguera <88061365+VmMad@users.noreply.github.com>
2026-01-09 16:23:33 +0100

    feat: add bg for fair acution

--- dapp/src/sections/FairAuction.tsx
+++ dapp/src/sections/FairAuction.tsx
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Activity, Clock, RecognizedBadge } from '@iota/apps-ui-icons';
+import Lottie from 'lottie-react';
 
+import fairAuctionAnimation from '@/animations/landing/fair-auction_bg_v1.json';
 import { CircleGradient } from '@/components/CircleGradient';
 import { IconCard } from '@/components/IconCard';
 
@@ -32,22 +34,41 @@ export function FairAuction() {
     return (
         <section className="relative overflow-hidden">
             <div className="container py-14 md:py-20">
-                <div className="relative flex flex-col md:flex-row gap-6 md:gap-[72px] py-lg md:py-[40px] items-center md:items-start bg-names-neutral-6 rounded-3xl px-xl md:px-2xl bg-[url('/landing-page/vector.png')] bg-no-repeat bg-cover bg-center">
-                    <CircleGradient position="top-left" />
-                    <div className="flex flex-col justify-center gap-xl md:gap-2xl flex-1">
-                        <div className="flex flex-col gap-xs">
-                            <h2 className="text-headline-lg text-names-primary-100">{TITTLE}</h2>
-                            <p className="text-body-lg text-names-neutral-70 w-full md:max-w-[428px]">
-                                {BODY}
-                            </p>
-                        </div>
-                        <div className="flex flex-col md:flex-row gap-lg">
-                            {FAIR_AUCTION.map((data, index) => (
-                                <IconCard key={index} {...data} />
-                            ))}
+                <div className="bg-names-neutral-6 rounded-3xl py-lg md:py-[40px] px-xl md:px-2xl relative">
+                    <div className="absolute inset-0 z-0 pointer-events-none select-none overflow-hidden rounded-3xl">
+                        <div className="h-full w-full ">
+                            <Lottie
+                                animationData={fairAuctionAnimation}
+                                loop={true}
+                                autoplay={true}
+                                style={{
+                                    height: '100%',
+                                }}
+                                rendererSettings={{
+                                    preserveAspectRatio: 'xMidYMid slice',
+                                }}
+                            />
                         </div>
                     </div>
-                    <img width={360} src="/landing-page/iota-news.png" />
+                    <CircleGradient position="top-left" />
+                    <div className="flex flex-col md:flex-row gap-6 md:gap-[72px] items-center md:items-start z-[1] relative">
+                        <div className="flex flex-col justify-center gap-xl md:gap-2xl flex-1">
+                            <div className="flex flex-col gap-xs">
+                                <h2 className="text-headline-lg text-names-primary-100">
+                                    {TITTLE}
+                                </h2>
+                                <p className="text-body-lg text-names-neutral-70 w-full md:max-w-[428px]">
+                                    {BODY}
+                                </p>
+                            </div>
+                            <div className="flex flex-col md:flex-row gap-lg">
+                                {FAIR_AUCTION.map((data, index) => (
+                                    <IconCard key={index} {...data} />
+                                ))}
+                            </div>
+                        </div>
+                        <img width={360} src="/landing-page/iota-news.png" />
+                    </div>
                 </div>
             </div>
         </section>
