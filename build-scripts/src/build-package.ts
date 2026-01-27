be43041420d4f602a6906747ec93d52383ad98aa
JCNoguera <88061365+VmMad@users.noreply.github.com>
2026-01-27 12:42:32 +0100

    chore: add missing license headers

--- build-scripts/src/build-package.ts
+++ build-scripts/src/build-package.ts
@@ -1,7 +1,7 @@
-#! /usr/bin/env tsx
 // Copyright (c) Mysten Labs, Inc.
 // Modifications Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+
 import { buildPackage } from './utils/buildPackage.js';
 
 buildPackage().catch((error) => {
