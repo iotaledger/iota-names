97548d3c05f53773e79aa7ef8519a7d3c731a29b
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2026-01-30 14:57:13 +0200

    revert enabling amplitude

--- dapp/src/lib/utils/analytics/amplitude.ts
+++ dapp/src/lib/utils/analytics/amplitude.ts
@@ -11,7 +11,9 @@ import { ampli } from './ampli';
 import { AMP_COOKIES_KEY } from './constants';
 import { contextEnrichmentPlugin } from './plugins/contextEnrichmentPlugin';
 
-const IS_ENABLED = true;
+const IS_ENABLED =
+    process.env.NEXT_PUBLIC_BUILD_ENV === 'production' &&
+    process.env.NEXT_PUBLIC_AMPLITUDE_ENABLED === 'true';
 
 /**
  * Check if user has previously given consent for cookies/tracking.
