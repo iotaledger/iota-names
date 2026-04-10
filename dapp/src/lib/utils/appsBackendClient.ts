7981675929a0e061d8b9e44d84f7bae10f05ea4d
panteley <panteleymonchuk@gmail.com>
2026-04-10 17:09:50 +0300

    revert "test e2e without apps abckend client"

--- dapp/src/lib/utils/appsBackendClient.ts
+++ dapp/src/lib/utils/appsBackendClient.ts
@@ -5,7 +5,4 @@ import { AppsBackendClient } from '@iota/apps-backend-client';
 
 export const appsBackendClient = new AppsBackendClient(process.env.NEXT_PUBLIC_APPS_BACKEND || '');
 
-// Only refresh features if we have a valid backend URL configured
-if (process.env.NEXT_PUBLIC_APPS_BACKEND) {
-    appsBackendClient.refreshFeatures();
-}
+appsBackendClient.refreshFeatures();
